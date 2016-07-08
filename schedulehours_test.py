535bd086fd9e1af805916d5daf71a372dddd2e90
César López Ramírez <cesar.lopez@somenergia.coop>
2016-07-08 09:26:38 +0200

    Added two slots case to yaml to html timetable

--- schedulehours_test.py
+++ schedulehours_test.py
@@ -5,11 +5,21 @@ from yamlns import namespace as ns
 def htmlTable(yaml):
     return(
             """<table>\n"""
-            """<tr><td></td><th colspan=1>dl</th></tr>\n"""
-            """<tr><td></td><th>T1</th>\n"""
+            """<tr>"""
+            """<td></td><th colspan={colspan}>dl</th>""".format(
+                colspan=len(yaml.torns)
+                )+""
             """</tr>\n"""
-            """<tr><th>{period}</th>\n"""
-            """<td class='{name}'>{properName}</td>\n"""
+            """<tr><td></td>"""
+            ""+("".join(["<th>{}</th>".format(t) for t in yaml.torns]))+""
+            """</tr>\n"""
+            """<tr><th>{period}</th>\n"""+""
+            "\n".join([
+                """<td class='{name}'>"""
+                """{properName}</td>""".format(
+                    name=name,
+                    properName=name.title())
+                    for name in yaml.timetable.dl[1]])+"\n"
             """</tr>\n"""
             """</table>"""
             ).format(
@@ -17,16 +27,6 @@ def htmlTable(yaml):
                 properName=yaml.timetable.dl[1][0].title(),
                 period="-".join(yaml.hores[0:2])
                 )
-    return """\
-<table>
-<tr><td></td><th colspan=1>dl</th></tr>
-<tr><td></td><th>T1</th>
-</tr>
-<tr><th>09:00-10:15</th>
-<td class='ana'>Ana</td>
-</tr>
-</table>"""
-
 
 
 class ScheduleHours_Test(unittest.TestCase):
@@ -56,13 +56,44 @@ class ScheduleHours_Test(unittest.TestCase):
             htmlTable(inputyaml), 
             """<table>\n"""
             """<tr><td></td><th colspan=1>dl</th></tr>\n"""
-            """<tr><td></td><th>T1</th>\n"""
+            """<tr><td></td><th>T1</th>"""
             """</tr>\n"""
             """<tr><th>09:00-10:15</th>\n"""
             """<td class='ana'>Ana</td>\n"""
             """</tr>\n"""
-            """</table>"""
-)
-
+            """</table>""")
+    def test_htmlTable_twoTelephonesOneTurnOneDay(self):
+        inputyaml=self.ns(u"""\
+            setmana: 2016-07-25
+            timetable:
+              dl:
+                1:
+                - ana
+                - jordi
+            hores:
+            - 09:00
+            - '10:15'
+            torns:
+            - T1
+            - T2
+            colors:
+              ana: 98bdc0
+              jordi: ff9999
+            extensions:
+              ana: 3181
+              jordi: 3183
+            """)
+        self.assertMultiLineEqual(
+            htmlTable(inputyaml),
+            """<table>\n"""
+            """<tr><td></td><th colspan=2>dl</th></tr>\n"""
+            """<tr><td></td><th>T1</th><th>T2</th>"""
+            """</tr>\n"""
+            """<tr><th>09:00-10:15</th>\n"""
+            """<td class='ana'>Ana</td>\n"""
+            """<td class='jordi'>Jordi</td>\n"""
+            """</tr>\n"""
+            """</table>""")
+        
 
 # vim: ts=4 sw=4 et
