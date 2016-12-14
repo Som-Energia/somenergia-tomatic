49a590006f5dd0bf7f4ee3382572c0c3163cd041
David García Garzón <voki@canvoki.net>
2016-12-14 22:03:13 +0100

    main function for the api launcher script

--- tomatic_api.py
+++ tomatic_api.py
@@ -2,3 +2,9 @@
 
 from  tomatic.api import app
 
+if __name__ == '__main__':
+    print app
+    for rule in app.url_map.iter_rules():
+        print rule
+    app.run(debug=True, host='0.0.0.0', processes=1)
+
