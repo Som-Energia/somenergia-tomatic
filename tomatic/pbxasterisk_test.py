036c753ddf99c16eed6746196fa139207d944811
David García Garzón <voki@canvoki.net>
2017-01-03 22:27:17 +0100

    remote as object, and restoring asterisk config

--- tomatic/pbxasterisk_test.py
+++ tomatic/pbxasterisk_test.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .pbxasterisk import Pbx, weekday, remotewrite, remoteread, remoterun
+from .pbxasterisk import Pbx, weekday, remotewrite, remoteread, remoterun, Remote
 import unittest
 from datetime import datetime, timedelta
 from yamlns import namespace as ns
@@ -13,25 +13,23 @@ except ImportError:
 
 class Remote_Test(unittest.TestCase):
 
+    def setUp(self):
+        self.user = config.pbx.scp.username
+        self.host = config.pbx.scp.pbxhost
+
     def test_remoterun(self):
-        result = remoterun(
-            config.pbx.scp.username,
-            config.pbx.scp.pbxhost,
-            "cat /etc/hosts")
+        result = remoterun(self.user, self.host, "cat /etc/hosts")
         self.assertIn('localhost', result)
 
     def test_remoteread(self):
-        content = remoteread(
-            config.pbx.scp.username,
-            config.pbx.scp.pbxhost,
-            "/etc/hosts")
+        content = remoteread(self.user, self.host, "/etc/hosts")
         self.assertIn("localhost", content)
 
     def test_remoteread_badPath(self):
         with self.assertRaises(IOError) as ctx:
             content = remoteread(
-                config.pbx.scp.username,
-                config.pbx.scp.pbxhost,
+                self.user,
+                self.host,
                 "/etc/badfile")
         self.assertEqual(str(ctx.exception),
             "[Errno 2] No such file")
@@ -40,31 +38,70 @@ class Remote_Test(unittest.TestCase):
         try:
             content = "Some content"
             remotewrite(
-                config.pbx.scp.username,
-                config.pbx.scp.pbxhost,
+                self.user,
+                self.host,
                 "borrame",
                 content)
             result = remoteread(
-                config.pbx.scp.username,
-                config.pbx.scp.pbxhost,
+                self.user,
+                self.host,
                 "borrame")
             self.assertEqual(result, content)
         finally:
             remoterun(
-                config.pbx.scp.username,
-                config.pbx.scp.pbxhost,
+                self.user,
+                self.host,
                 'rm borrame'
                 )
+
+    def test_remote_run(self):
+        with Remote(self.user, self.host) as remote:
+            result = remote.run("cat /etc/hosts")
+
+        self.assertIn('localhost', result)
+
+    def test_remote_read(self):
+        with Remote(self.user, self.host) as remote:
+            content = remote.read("/etc/hosts")
+
+        self.assertIn("localhost", content)
+
+    def test_remote_read_badPath(self):
+        with Remote(self.user, self.host) as remote:
+            with self.assertRaises(IOError) as ctx:
+                content = remote.read("/etc/badfile")
+        self.assertEqual(str(ctx.exception),
+            "[Errno 2] No such file")
+
+    def test_remote_write(self):
+        with Remote(self.user, self.host) as remote:
+            try:
+                content = "Some content"
+                remote.write("borrame", content)
+                result = remote.read("borrame")
+                self.assertEqual(result, content)
+            finally:
+                remote.run('rm borrame')
             
 
 
 class Pbx_Test(unittest.TestCase):
 
     def setUp(self):
+        self.previousConfig = None
+        self.remote = Remote(
+            config.pbx.scp.username,
+            config.pbx.scp.pbxhost,
+            )
+        self.previousConfig = self.remote.read(config.pbx.scp.path)
         now = datetime.now()
         self.today = weekday(now)
         self.currentHour = now.hour
         self.nextday = weekday(now+timedelta(days=1))
+
+    def tearDown(self):
+        if self.previousConfig is None:
+            self.remote.write(config.pbx.scp.path, self.previousConfig)
 
     def pbx(self):
         return Pbx()
