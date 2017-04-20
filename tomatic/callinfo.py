597fafec0497441b8dce2dd316feb74148c36e56
oriolpiera <oriolpiera@somenergia.coop>
2017-04-20 18:50:13 +0200

    getByPhone test

--- tomatic/callinfo.py
+++ tomatic/callinfo.py
@@ -9,45 +9,42 @@ class CallInfo(object):
     def __init__(self, O, anonymize=False):
         self.O = O
         self._anonymize = anonymize
-	self._partnerData = ns.loads("""partner: 
-  lang:
-  name:
-  city:
-  email:
-  polisses_ids:
-  provincia:
- """)
 
     def anonymize(self, string):
         if not self._anonymize: return string
-        return string[:1]+"..."+string[-2:]
+        return string[:2]+"..."+string[-2:]
 
-    def searchAddresByPhone(self, phone):
-        return self.O.ResPartnerAddress.search([('phone','=',phone)])
-
-    def searchAddresByMobile(self, phone):
-        return self.O.ResPartnerAddress.search([('mobile','=',phone)])
+    def addressByPhone(self, phone):
+        return self.O.ResPartnerAddress.search([
+            '|',
+            ('phone','=',phone),
+            ('mobile','=',phone),
+            ])
 
     def searchPartnerByAddressId(self, address_ids):
-        partner_ids = []
-        for address_id in address_ids:
-            partner_address_register = self.O.ResPartnerAddress.read(address_id, ['id', 'partner_id'])
-            if not partner_address_register or partner_address_register['partner_id'] == False: 
-                continue
-            partner_ids.append(partner_address_register['partner_id'][0])
-        return partner_ids
+        return [
+            address['partner_id'][0]
+            for address
+            in self.O.ResPartnerAddress.read(address_ids, ['id', 'partner_id'])
+            if address and address['partner_id']
+            ]
     
     def getPartnerData(self, partner_id):
-	partner_data = self.O.ResPartner.read(partner_id, [])
-	if not partner_data:
-		return self._partnerData
-	self._partnerData['partner']['lang'] = partner_data[0]['lang']
-	self._partnerData['partner']['name'] = self.anonymize(partner_data[0]['name'])
-	self._partnerData['partner']['city'] = partner_data[0]['city']
-	self._partnerData['partner']['email'] = self.anonymize(partner_data[0]['www_email'])
-        self._partnerData['partner']['polisses_ids'] = partner_data[0]['polisses_ids']
-        self._partnerData['partner']['provincia'] = partner_data[0]['www_provincia'][1]['name']
+        partner_data = self.O.ResPartner.read(partner_id, [])
+        partner_data = ns(partner_data[0])
+        result = ns(partner=ns())
+        result.partner.update(
+            lang = partner_data.lang,
+            name = self.anonymize(partner_data.name),
+            city = partner_data.city,
+            email = self.anonymize(partner_data.www_email),
+            polisses_ids = partner_data.polisses_ids,
+            provincia = partner_data.www_provincia[1]['name'],
+            )
 
-	return self._partnerData
+        return result
 
+    def getByPhone(self, phone):
+        return ns(partner=ns())
+ 
 # vim: ts=4 sw=4 et
--- tomatic/callinfo_test.py
+++ tomatic/callinfo_test.py
@@ -8,49 +8,64 @@ try:
 except ImportError:
     dbconfig = None
 from yamlns import namespace as ns
+import b2btest
     
 @unittest.skipIf(not dbconfig or not dbconfig.ooop,
     "Requires configuring dbconfig.ooop")
 class CallInfo_Test(unittest.TestCase):
 
+    def assertNsEqual(self, dict1, dict2):
+        def parseIfString(nsOrString):
+            if type(nsOrString) in (dict, ns):
+                return nsOrString
+            return ns.loads(nsOrString)
+
+        def sorteddict(d):
+            if type(d) not in (dict, ns):
+                return d
+            return ns(sorted(
+                (k, sorteddict(v))
+                for k,v in d.items()
+                ))
+        dict1 = sorteddict(parseIfString(dict1))
+        dict2 = sorteddict(parseIfString(dict2))
+
+        return self.assertMultiLineEqual(dict1.dump(), dict2.dump())
+
     def setUp(self):
-        pass
+        self.maxDiff = None
+        self.b2bdatapath = "testdata"
 
     def ns(self,content):
-	return ns.loads(content)
+        return ns.loads(content)
 
     @classmethod
     def setUpClass(cls):
         cls.O = ooop.OOOP(**dbconfig.ooop)
 
-    def test_searchAddressByPhone_whenMatchesNone(self):
+    def test_addressByPhone_whenMatchesNone(self):
         info = CallInfo(self.O)
-        ids = info.searchAddresByPhone('badphone')
+        ids = info.addressByPhone('badphone')
         self.assertEqual(ids, [])
 
-    def test_searchAddressByPhone_whenMatchesOnePhone(self):
+    def test_addressByPhone_whenMatchesOnePhone(self):
         info = CallInfo(self.O)
-        ids = info.searchAddresByPhone('933730976')
+        ids = info.addressByPhone('933730976')
         self.assertEqual(ids, [12073])
 
-    def test_searchAddressByPhone_whenMatchesMoreThanOnePhone(self):
+    def test_addressByPhone_whenMatchesMoreThanOnePhone(self):
         info = CallInfo(self.O)
-        ids = info.searchAddresByPhone('659509872')
+        ids = info.addressByPhone('659509872')
         self.assertEqual(ids, [2286, 42055, 43422])
 
-    def test_searchAddressByMobile_whenMatchesNone(self):
+    def test_addressByPhone_whenMatchesOneMobile(self):
         info = CallInfo(self.O)
-        ids = info.searchAddresByMobile('badphone')
-        self.assertEqual(ids, [])
-
-    def test_searchAddressByMobile_whenMatchesOnePhone(self):
-        info = CallInfo(self.O)
-        ids = info.searchAddresByMobile('699515879')
+        ids = info.addressByPhone('699515879')
         self.assertEqual(ids, [34])
 
-    def test_searchAddressByMobile_whenMatchesMoreThanOnePhone(self):
+    def test_addressByPhone_whenMatchesMoreThanOneMobile(self):
         info = CallInfo(self.O)
-        ids = info.searchAddresByMobile('630079522')
+        ids = info.addressByPhone('630079522')
         self.assertEqual(ids, [33,42])
 
     def test_searchPartnerByAddressId_whenMatchesNone(self):
@@ -83,32 +98,25 @@ class CallInfo_Test(unittest.TestCase):
         partner_ids = info.searchPartnerByAddressId([2286, 42055, 43422, 999999999])
         self.assertEqual(partner_ids, [410, 39933, 41193])
 
-    '''def test_getPartnerData_whenEmpty(self):
-	info = CallInfo(self.O)
-	partner_data = info.getPartnerData([0])
-	self.assertEqual(partner_data, self.ns("""partner: 
-  lang:
-  name:
-  city:
-  email:
-  polisses_ids:
-  provincia:
- """))'''
-	
-
     def test_getPartnerData_whenMatchesOne(self):
-	info = CallInfo(self.O, anonimize=True)
-	partner_data = info.getPartnerData([410])
-        self.assertEqual(partner_data, self.ns("""partner:
-  'lang': 'ca_ES' 
-  'name': 'Ju...ol'
-  'city': 'Vilanova de Bellpuig'
-  'email': 'or...op'
-  'polisses_ids': 
-  - 155
-  - 56427
-  'provincia': 'Lleida'
-"""))
+        info = CallInfo(self.O, anonymize=True)
+        partner_data = info.getPartnerData([410])
+        self.assertNsEqual(partner_data, """\
+            partner:
+              'lang': 'ca_ES' 
+              'name': 'Ju...ol'
+              'city': 'Vilanova de Bellpuig'
+              'email': 'or...op'
+              'polisses_ids': 
+              - 155
+              - 56427
+              'provincia': 'Lleida'
+            """)
+
+    def test_getByPhone(self):
+        info = CallInfo(self.O)
+        data = info.getByPhone("620471117")
+        self.assertB2BEqual(data.dump())
 
 unittest.TestCase.__str__ = unittest.TestCase.id
 
