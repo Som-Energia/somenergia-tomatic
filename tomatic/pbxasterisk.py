7f7e567848d3e8484602452c5ff39f4e1c379b75
David García Garzón <voki@canvoki.net>
2017-04-26 22:01:42 +0200

    pbxasterisk: the working real one

---
+++ tomatic/pbxasterisk.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+from schedulestorage import Storage
+from dbasterisk import DbAsterisk
+from scheduling import choosers, Scheduling
+import datetime
+from yamlns import namespace as ns
+
+class PbxAsterisk(object):
+
+    def __init__(self, path, *dbargs, **dbkwd):
+        self.storage = Storage(path)
+        self.db = DbAsterisk(*dbargs, **dbkwd)
+
+    def _currentSched(self, when=None):
+        when = when or datetime.datetime.now()
+        week, dow, time = choosers(when)
+        try:
+            yaml=self.storage.load(week)
+        except KeyError:
+            return None
+        return Scheduling(yaml)
+
+    def setSchedQueue(self, when):
+        sched = self._currentSched(when)
+        if sched is None:
+            self.db.setQueue('somenergia', [])
+            return
+        week, dow, time = choosers(when)
+        self.db.setQueue('somenergia', [
+            sched.extension(name)
+            for name in sched.peekQueue(dow, time)
+        ])
+
+    def currentQueue(self):
+        sched = self._currentSched()
+        if sched is None:
+            return []
+        return [
+            ns(
+                key=sched.extensionToName(extension),
+                paused=bool(paused),
+            )
+            for extension, paused
+            in self.db.queue('somenergia')
+        ]
+
+    
+    def pause(self, name):
+        sched = self._currentSched()
+        self.db.pause('somenergia', sched.extension(name))
+    def resume(self, name):
+        sched = self._currentSched()
+        self.db.resume('somenergia', sched.extension(name))
+
+    def addLine(self, name):
+        sched = self._currentSched()
+        self.db.add('somenergia', sched.extension(name))
+
+
+
+# vim: ts=4 sw=4 et
---
+++ tomatic/pbxasterisk_test.py
@@ -0,0 +1,341 @@
+# -*- coding: utf-8 -*-
+
+from .pbxasterisk import PbxAsterisk
+from scheduling import weekday, weekstart
+from schedulestorage import Storage
+import unittest
+from datetime import datetime, timedelta
+from yamlns import namespace as ns
+import tempfile
+import os
+
+class PbxAsterisk_Test(unittest.TestCase):
+
+    def setUp(self):
+        self.now = now = datetime.now()
+        self.today = weekday(now)
+        self.monday = weekstart(now)
+        self.currentHour = now.hour
+        self.nextday = weekday(now+timedelta(days=1))
+        self.path = tempfile.mkdtemp()
+
+    def tearDown(self):
+        os.system('rm -rf {}'.format(self.path))
+
+    def pbx(self):
+        return PbxAsterisk(self.path, 'sqlite', 'demo.sqlite', create_db=True)
+
+    def setupScheduling(self, yaml):
+        sched = ns.loads(yaml)
+        storage = Storage(self.path)
+        sched.week = str(self.monday.date())
+        storage.save(sched)
+
+    def test_currentQueue_noConfiguration(self):
+        pbx = self.pbx()
+        result = pbx.currentQueue()
+        self.assertEqual(result, [])
+
+    def test_currentQueue_oneSlot_oneTurn(self):
+        self.setupScheduling(u"""\
+            timetable:
+              {today}:
+              -
+                - cesar
+            hours:
+            - '00:00'
+            - '23:59'
+
+            extensions:
+              cesar: 200
+            """.format(
+                today=self.today
+            ))
+        pbx = self.pbx()
+        pbx.setSchedQueue(self.now)
+        self.assertEqual(pbx.currentQueue(), [
+            ns( key='cesar', paused=False),
+            ])
+
+    def test_currentQueue_oneSlot_twoTurns(self):
+        self.setupScheduling(u"""\
+            timetable:
+              {today}:
+              -
+                - cesar
+                - eduard
+            hours:
+            - '00:00'
+            - '23:59'
+
+            extensions:
+              cesar: 200
+              eduard: 201
+            """.format(
+                today=self.today
+            ))
+        pbx = self.pbx()
+        pbx.setSchedQueue(self.now)
+        self.assertEqual(pbx.currentQueue(), [
+            ns( key='cesar', paused=False),
+            ns( key='eduard', paused=False),
+            ])
+
+    def test_currentQueue_twoTimes(self):
+        self.setupScheduling(u"""\
+            timetable:
+              {today}:
+              -
+                - cesar
+              -
+                - eduard
+            hours:
+            - '00:00'
+            - '{splithour:02d}:00'
+            - '23:59'
+
+            extensions:
+              cesar: 200
+              eduard: 201
+            """.format(
+                splithour=self.currentHour,
+                today=self.today,
+            ))
+        pbx = self.pbx()
+        pbx.setSchedQueue(self.now)
+        self.assertEqual(pbx.currentQueue(), [
+            ns( key='eduard', paused=False),
+            ])
+
+    def test_currentQueue_beforeTime(self):
+        self.setupScheduling(u"""\
+            timetable:
+              {today}:
+              -
+                - cesar
+            hours:
+            - '23:58'
+            - '23:59'
+
+            extensions:
+              cesar: 200
+            """.format(
+                today=self.today,
+            ))
+        pbx = self.pbx()
+        pbx.setSchedQueue(self.now)
+        self.assertEqual(pbx.currentQueue(), [
+            ])
+
+    def test_currentQueue_afterTime(self):
+        self.setupScheduling(u"""\
+            timetable:
+              {today}:
+              -
+                - cesar
+            hours:
+            - '00:00'
+            - '00:01'
+
+            extensions:
+              cesar: 200
+            """.format(
+                today=self.today,
+            ))
+        pbx = self.pbx()
+        pbx.setSchedQueue(self.now)
+        self.assertEqual(pbx.currentQueue(), [
+            ])
+
+
+    def test_pause_withOne(self):
+        self.setupScheduling(u"""\
+            timetable:
+              {today}:
+              -
+                - cesar
+            hours:
+            - '00:00'
+            - '23:59'
+
+            extensions:
+              cesar: 200
+            """.format(
+                today=self.today
+            ))
+        pbx = self.pbx()
+        pbx.setSchedQueue(self.now)
+        pbx.pause('cesar')
+        self.assertEqual(pbx.currentQueue(), [
+            ns( key='cesar', paused=True),
+            ])
+
+    def test_pause_withTwoLines(self):
+        self.setupScheduling(u"""\
+            timetable:
+              {today}:
+              -
+                - cesar
+                - eduard
+            hours:
+            - '00:00'
+            - '23:59'
+
+            extensions:
+              cesar: 200
+              eduard: 201
+            """.format(
+                today=self.today
+            ))
+        pbx = self.pbx()
+        pbx.setSchedQueue(self.now)
+        pbx.pause('cesar')
+        self.assertEqual(pbx.currentQueue(), [
+            ns( key='cesar', paused=True),
+            ns( key='eduard', paused=False),
+            ])
+
+    def test_resume(self):
+        self.setupScheduling(u"""\
+            timetable:
+              {today}:
+              -
+                - cesar
+            hours:
+            - '00:00'
+            - '23:59'
+
+            extensions:
+              cesar: 200
+            """.format(
+                today=self.today
+            ))
+        pbx = self.pbx()
+        pbx.setSchedQueue(self.now)
+        pbx.pause('cesar')
+        pbx.resume('cesar')
+        self.assertEqual(pbx.currentQueue(), [
+            ns( key='cesar', paused=False),
+            ])
+
+    def test_resume_afterDoublePause(self):
+        self.setupScheduling(u"""\
+            timetable:
+              {today}:
+              -
+                - cesar
+            hours:
+            - '00:00'
+            - '23:59'
+
+            extensions:
+              cesar: 200
+            """.format(
+                today=self.today
+            ))
+        pbx = self.pbx()
+        pbx.setSchedQueue(self.now)
+        pbx.pause('cesar')
+        pbx.pause('cesar')
+        pbx.resume('cesar')
+        self.assertEqual(pbx.currentQueue(), [
+            ns( key='cesar', paused=False),
+            ])
+
+    def test_currentQueue_withKeyFormat(self):
+        self.setupScheduling(u"""\
+            timetable:
+              {today}:
+                1:
+                - cesar
+                - eduard
+            hours:
+            - '00:00'
+            - '23:59'
+
+            extensions:
+              cesar: 200
+              eduard: 201
+            """.format(
+                today=self.today
+            ))
+        pbx = self.pbx()
+        pbx.setSchedQueue(self.now)
+        self.assertEqual(pbx.currentQueue(), [
+            ns( key='cesar', paused=False),
+            ns( key='eduard', paused=False),
+            ])
+
+    def test_currentQueue_beforeTime_listFormat(self):
+        self.setupScheduling(u"""\
+            timetable:
+              {today}:
+              -
+                - cesar
+            hours:
+            - '23:58'
+            - '23:59'
+
+            extensions:
+              cesar: 200
+            """.format(
+                today=self.today,
+            ))
+        pbx = self.pbx()
+        pbx.setSchedQueue(self.now)
+        self.assertEqual(pbx.currentQueue(), [
+            ])
+
+    def test_currentQueue_afterTime_listFormat(self):
+        pbx = self.pbx()
+        self.setupScheduling(u"""\
+            timetable:
+              {today}:
+              -
+                - cesar
+            hours:
+            - '00:00'
+            - '00:01'
+
+            extensions:
+              cesar: 200
+            """.format(
+                today=self.today,
+            ))
+        self.assertEqual(pbx.currentQueue(), [
+            ])
+
+    def test_addLine(self):
+        self.setupScheduling(u"""\
+            timetable:
+              {today}:
+              -
+                - cesar
+                - eduard
+            hours:
+            - '00:00'
+            - '23:59'
+
+            extensions:
+              cesar: 200
+              eduard: 201
+              david: 202
+            """.format(
+                today=self.today
+            ))
+        pbx = self.pbx()
+        pbx.setSchedQueue(self.now)
+        pbx.addLine('david')
+        self.assertEqual(pbx.currentQueue(), [
+            ns( key='cesar', paused=False),
+            ns( key='eduard', paused=False),
+            ns( key='david', paused=False),
+            ])
+
+
+unittest.TestCase.__str__ = unittest.TestCase.id
+ 
+if __name__ == "__main__":
+    unittest.main()
+
+# vim: ts=4 sw=4 et
