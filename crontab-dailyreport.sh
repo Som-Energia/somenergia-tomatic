7ffc144100cf5d41e96a8fc2e9c45dffdb9b05d8
proig <pere.roig@somenergia.coop>
2018-02-26 17:45:16 +0100

    nou formulari de temes

--- crontab-dailyreport.sh
+++ crontab-dailyreport.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-PYTHONIOENCODING=utf8 ./tomatic_calls.py summary | while read a; do ./tomatic_says.py "$a"; done
+PYTHONIOENCODING=utf8 ./tomatic_calls.py summary |
+while read a
+do
+	./tomatic_says.py "$a"
+done
+./tomatic_says.py "Recordeu anotar les trucades: https://intranet.helpscoutdocs.com/article/749-formulari-telefon-inquietuds-clients"
 
 
