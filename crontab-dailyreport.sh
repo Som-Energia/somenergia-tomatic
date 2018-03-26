761d916c93949d5ac5f7a3380161609fdb7ed022
proig <tomatic@somenergia.coop>
2018-03-26 15:31:33 +0200

    URL per anotar trucades a cada torn

--- crontab-dailyreport.sh
+++ crontab-dailyreport.sh
@@ -5,6 +5,5 @@ while read a
 do
 	./tomatic_says.py "$a"
 done
-./tomatic_says.py "Recordeu anotar les trucades: https://intranet.helpscoutdocs.com/article/749-formulari-telefon-inquietuds-clients"
 
 
--- crontab-warnturn.sh
+++ crontab-warnturn.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ./tomatic_says.py "Canvi de torn en 5m: $(tomatic_rtqueue.py preview --time $(date -d '10 minutes' +'%H:%M') )"
+./tomatic_says.py "Recordeu anotar les trucades: https://intranet.helpscoutdocs.com/article/749-formulari-telefon-inquietuds-clients"
 
 
