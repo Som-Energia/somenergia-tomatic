502845d63b3df66caf1924bb3284829290acd94a
Marta <marta.fernandez@somenergia.coop>
2020-03-02 12:50:59 +0100

    refactor: name O to Client

--- tomatic/claims.py
+++ tomatic/claims.py
@@ -4,14 +4,14 @@ from yamlns import namespace as ns
 
 class Claims(object):
 
-    def __init__(self, O):
-        self.O = O
+    def __init__(self, Client):
+        self.Client = Client
         config = ns.load('config.yaml')
 
         self.assign_user = config.assign_user
 
     def get_claims(self):
-        reclamacio_obj = self.O.GiscedataSubtipusReclamacio
+        reclamacio_obj = self.Client.GiscedataSubtipusReclamacio
 
         reclamacions = []
         all_reclamacio_ids = reclamacio_obj.search()
@@ -32,11 +32,11 @@ class Claims(object):
         return reclamacions
 
     def create_atc_case(self, data_crm, data_atc):
-        crm_obj = self.O.CrmCase
+        crm_obj = self.Client.CrmCase
         crm_id = crm_obj.create(data_crm)
 
         data_atc['crm_id'] = crm_id
-        atc_obj = self.O.GiscedataAtc
+        atc_obj = self.Client.GiscedataAtc
         case = atc_obj.create(data_atc)
 
         return case.id
