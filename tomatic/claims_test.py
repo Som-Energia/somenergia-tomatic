5eec8a7790c86734942e600e7b7b30a3a19bc67b
Marta <marta.fernandez@somenergia.coop>
2020-03-02 08:57:18 +0100

    test_createAtcCase basic

--- tomatic/claims_test.py
+++ tomatic/claims_test.py
@@ -25,6 +25,7 @@ class Claims_Test(unittest.TestCase):
         if not dbconfig.erppeek:
             return
         cls.O = erppeek.Client(**dbconfig.erppeek)
+        cls.data_atc = dbconfig.data_atc
 
     def test_getAllClaims(self):
         claims = Claims(self.O)
@@ -33,6 +34,20 @@ class Claims_Test(unittest.TestCase):
         nombre_reclamacions = reclamacio_obj.count()
 
         self.assertEqual(len(reclamacions), nombre_reclamacions)
+
+    @unittest.skip('WIP')
+    def test_createAtcCase_ok(self):
+        # with discarded_transaction(O) as t:
+        data_crm = {
+            'description': 'Some tests.',
+            'section_id': 28,
+            'name': 'Descripcio del cas'
+        }
+
+        claims = Claims(self.O)
+        case_id = claims.create_atc_case(data_crm, self.data_atc)
+
+        self.assertEqual(case_id, 5)
 
 
 # vim: et ts=4 sw=4
