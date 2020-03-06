c469efdb8437ab090642f736049875d6c4bffbdf
David García Garzón <voki@canvoki.net>
2020-03-06 05:45:46 +0100

    tomatic_mergeshifts: using loadSum

--- tomatic_mergeshifts.py
+++ tomatic_mergeshifts.py
@@ -1,26 +1,17 @@
 #!/usr/bin/env python
 
 import sys
+from tomatic.shiftload import loadSum
+from yamlns import namespace as ns
 
 def main():
-    '''TODO: sanitize'''
-    first_dict_yaml = sys.argv[1]
-    second_dict_yaml = sys.argv[2]
-
-    from yamlns import namespace as ns
-
-    first_dict = ns.load(first_dict_yaml)
-    second_dict = ns.load(second_dict_yaml)
-
-    merged_dict = first_dict
-    for k2,v2 in second_dict.items():
-        v1 = first_dict.get(k2,0)
-        merged_dict[k2] = v1 + v2
-        #print('{}: {} + {} = {}'.format(k2,v1,v2,merged_dict[k2]))
-
-    print(merged_dict.dump())
-
-    return merged_dict
+    result = loadSum(*(
+        ns.load(filename)
+        for filename in sys.argv[1:]
+    ))
+    print(result.dump())
 
 if __name__ == "__main__":
-	main()
+    main()
+
+# vim: et ts=4 sw=4
