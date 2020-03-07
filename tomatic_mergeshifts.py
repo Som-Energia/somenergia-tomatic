8390b23988cb63e6773504dd767f5644c26656a1
David García Garzón <voki@canvoki.net>
2020-03-07 12:56:26 +0100

    tomatic_mergeshifts: min and subst subcommands

--- tomatic_mergeshifts.py
+++ tomatic_mergeshifts.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
 
 import sys
-from tomatic.shiftload import loadSum
+from tomatic.shiftload import loadSum, loadMin, loadSubstract
 from yamlns import namespace as ns
 
 def main():
-    result = loadSum(*(
-        ns.load(filename)
-        for filename in sys.argv[1:]
-    ))
+    if sys.argv[1] == 'min':
+        result = loadMin(*(
+            ns.load(filename)
+            for filename in sys.argv[2:]
+        ))
+    elif sys.argv[1] == 'subs':
+        result = loadSum(*(
+            ns.load(filename)
+            for filename in sys.argv[2:4]
+        ))
+    else:
+        result = loadSum(*(
+            ns.load(filename)
+            for filename in sys.argv[1:]
+        ))
+
     result = ns((p,round(v,1)) for p,v in sorted(result.items()))
     print(result.dump())
 
