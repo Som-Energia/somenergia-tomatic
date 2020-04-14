4b18f829d6d395fd0c7252c332c56bab66009ff7
Marta <marta.fernandez@somenergia.coop>
2020-04-14 10:17:31 +0200

    add more info in create_atc_case

--- tomatic/claims.py
+++ tomatic/claims.py
@@ -1,6 +1,47 @@
 # -*- encoding: utf-8 -*-
+import re
 from yamlns import namespace as ns
-import time
+from consolemsg import error
+
+PHONE = 2
+COMERCIALIZADORA = '06'
+
+def partnerId(Client, partner):
+    partner_model = Client.ResPartner
+    return partner_model.browse([('ref', '=', partner)])[0].id
+
+def partnerAddress(Client, partner_id):
+    partner_address_model = Client.ResPartnerAddress
+    return partner_address_model.read(
+        [('partner_id', '=', partner_id)],
+        ['id', 'state_id']
+    )[0]
+
+def contractId(Client, contract):
+    contract_model = Client.GiscedataPolissa
+    return contract_model.browse([("name", "=", contract)])[0].id
+
+
+def cupsId(Client, cups):
+    cups_model = Client.GiscedataCupsPs
+    return cups_model.browse([('name', '=', cups)])[0].id
+
+
+def userId(Client, person):
+    users_model = Client.ResUsers
+    try:
+        user_id = users_model.browse([('login', '=', person)])[0].id
+        return user_id
+    except IndexError as e:
+        error("User {} not found: {}", person, e)
+
+
+def resultat(Client, procedente, improcedente):
+    if procedente:
+        return '01'
+    if improcedente:
+        return '02'
+    return '03'
 
 
 class Claims(object):
@@ -37,15 +78,59 @@ class Claims(object):
 
         return claims
 
-    def create_atc_case(self, data_crm, data_atc):
-        crm_obj = self.Client.CrmCase
-        crm_id = crm_obj.create(data_crm)
 
+    def create_atc_case(self, case):
+        '''
+        Expected case:
+
+        namespace(
+            person:
+                - date: D-M-YYYY H:M:S
+                person: person
+                reason: '[section.name] claim.name - claim.desc'
+                partner: partner number
+                contract: contract number
+                procedente: ''
+                improcedente: ''
+                solved: ''
+                user: section.name
+                cups: cups number
+                observations: comments
+                - ...
+            ...
+        )
+        '''
+
+        partner_id = partnerId(self.Client, case.partner)
+        partner_address = partnerAddress(self.Client, partner_id)
+
+        data_crm = {
+            'section_id': int(re.search('\d+', case.reason).group()),
+            'name': case.reason.split("-")[1].strip(),
+            'description': case.observations,
+            'canal_id': PHONE,
+            'polissa_id': contractId(self.Client, case.contract),
+            'partner_id': partner_id,
+            'partner_address_id': partner_address.get('id'),
+            'state': 'done' if case.solved else 'open'
+        }
+        # TODO: 'user_id': userId(self.Client, case.person)
+        crm_obj = self.Client.CrmCase
+        crm_id = crm_obj.create(data_crm).id
+
+        data_atc = {
+            'provincia': partner_address.get('state_id')[0],
+            'total_cups': 1,
+            'cups_id': cupsId(self.Client, case.cups),
+            'subtipus_id': int(re.search('\d+', case.reason).group()),
+            'reclamante': COMERCIALIZADORA,
+            'resultat': resultat(self.Client, case.procedente, case.improcedente),
+        }
         data_atc['crm_id'] = crm_id
+
         atc_obj = self.Client.GiscedataAtc
         case = atc_obj.create(data_atc)
 
         return case.id
-
 
 # vim: et ts=4 sw=4
--- tomatic/claims_test.py
+++ tomatic/claims_test.py
@@ -1,8 +1,12 @@
 # -*- encoding: utf-8 -*-
 
-import unittest
 import contextlib
+import unittest
+import os
+from consolemsg import error
+from datetime import datetime
 from erppeek_wst import ClientWST
+from yamlns import namespace as ns
 from .claims import Claims
 try:
     import dbconfig
@@ -43,20 +47,26 @@ class Claims_Test(unittest.TestCase):
 
         self.assertEqual(len(reclamacions), nombre_reclamacions)
 
-    @unittest.skip("WIP: how to undo transaction")
+    # @unittest.skip("WIP: how to undo transaction")
     def test_createAtcCase_ok(self):
-        with discarded_transaction(self.Client) as t:
-            data_crm = {
-                'description': 'Some tests.',
-                'section_id': 28,
-                'name': 'Descripcio del cas'
-            }
+        today = datetime.today()
+        file_name = "atc_cases/{}{}{}.yaml".format(
+            today.year,
+            today.month if today.month//10 != 0 else "0{}".format(today.month),
+            today.day if today.day//10 != 0 else "0{}".format(today.day)
+        )
+        if not os.path.isfile(file_name):
+            error("The file {} does not exists", file_name)
+        else:
+            atc_cases = ns.load(file_name)
 
-            claims = Claims(t)
-            case_id = claims.create_atc_case(data_crm, self.data_atc)
-
-            last_atc_case_id = self.Client.GiscedataAtc.search()[-1]
-            self.assertEqual(case_id, last_atc_case_id)
+            with discarded_transaction(self.Client) as t:
+                for person in atc_cases:
+                    for case in atc_cases[person]:
+                        claims = Claims(t)
+                        case_id = claims.create_atc_case(case)
+                        last_atc_case_id = t.GiscedataAtc.search()[-1]
+                        self.assertEqual(case_id, last_atc_case_id)
 
 
 # vim: et ts=4 sw=4
