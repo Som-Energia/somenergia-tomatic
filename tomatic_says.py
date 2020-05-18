d99fe545d8dc8ee41d69226634a26572e4e96a37
David García Garzón <voki@canvoki.net>
2020-05-18 11:55:12 +0200

    tomatic_says: conversation ID in config.yaml

--- tomatic_says.py
+++ tomatic_says.py
@@ -1,48 +1,58 @@
 #!/usr/bin/env python3
-from __future__ import print_function
 import asyncio
 
+from hangups import (
+    ChatMessageSegment,
+)
 import hangups
+from hangups.hangouts_pb2 import (
+    SendChatMessageRequest,
+    EventRequestHeader,
+    ConversationId,
+    MessageContent,
+)
 import sys
-import hangups.hangouts_pb2 as _
 import appdirs
 import os
-import dbconfig
+from yamlns import namespace as ns
+from consolemsg import success, step
 
-CONVERSATION_ID = 'UgwbNDnElaQHezX-qXN4AaABAQ' # it
-CONVERSATION_ID = 'UgzYiKUaJZ1yCpn3R4J4AaABAQ' # petit dejuner
-MESSAGE = ' '.join(sys.argv[1:])
-if not MESSAGE:
-    MESSAGE = sys.stdin.read()
+config = ns.load('config.yaml')
+
+message = ' '.join(sys.argv[1:])
+if not message:
+    message = sys.stdin.read()
 dirs = appdirs.AppDirs('hangups', 'hangups')
-REFRESH_TOKEN_PATH = os.path.join(dirs.user_cache_dir, 'refresh_token.txt')
-print(MESSAGE)
+token_path = os.path.join(dirs.user_cache_dir, 'refresh_token.txt')
+step(message)
 
-@asyncio.coroutine
-def send_message(client):
-    request = _.SendChatMessageRequest(
+
+async def send_message(client):
+    request = SendChatMessageRequest(
         request_header = client.get_request_header(),
-        event_request_header = _.EventRequestHeader(
-            conversation_id = _.ConversationId(
-                id=CONVERSATION_ID
+        event_request_header = EventRequestHeader(
+            conversation_id = ConversationId(
+                id = config.hangoutChannel,
             ),
             client_generated_id=client.get_client_generated_id(),
         ),
-        message_content = _.MessageContent(
-            segment=[hangups.ChatMessageSegment(MESSAGE).serialize()],
+        message_content = MessageContent(
+            segment=[hangups.ChatMessageSegment(message).serialize()],
         ),
     )
-    yield from client.send_chat_message(request)
-    yield from client.disconnect()
+    await client.send_chat_message(request)
+    await client.disconnect()
 
 
 def main():
-    cookies = hangups.auth.get_auth_stdin(REFRESH_TOKEN_PATH)
+    cookies = hangups.auth.get_auth_stdin(token_path)
     client = hangups.Client(cookies)
-    client.on_connect.add_observer(lambda: asyncio.async(send_message(client)))
+    client.on_connect.add_observer(lambda: asyncio.ensure_future(send_message(client)))
     loop = asyncio.get_event_loop()
     loop.run_until_complete(client.connect())
+    success('Sent')
 
 
 if __name__ == '__main__':
-	main()
+    main()
+
