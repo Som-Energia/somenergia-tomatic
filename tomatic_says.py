1eaaa16c4b66c4069c5b876e07d8909c66d86c7d
David García Garzón <voki@canvoki.net>
2020-06-04 09:58:39 +0200

    tomatic_says: no conversation match, list all

--- tomatic_says.py
+++ tomatic_says.py
@@ -56,7 +56,8 @@ async def resolveChannel(client, channel):
     user_list, conversation_list = (
         await hangups.build_user_conversation_list(client)
     )
-    for user in user_list.get_all():
+    all_users = user_list.get_all()
+    for user in all_users:
         if channel in user.emails:
             step("Sending message to {.full_name} <{}>", user, channel)
             return await open_conversation(client, "Tomatic", user.id_.gaia_id)
@@ -87,11 +88,8 @@ async def resolveChannel(client, channel):
 
     out('{} known conversations'.format(len(all_conversations)))
     for conversation in all_conversations:
-        if conversation.name:
-            name = conversation.name
-        else:
-            name = 'Unnamed conversation ({})'.format(conversation.id_)
-        out('    {}'.format(name))
+        name = conversation.name or 'Unnamed Conversation'
+        out('    {} ({})'.format(name, conversation.id_))
 
 async def send_message(client, channel, message):
     step("Resolving channel: {}", channel)
