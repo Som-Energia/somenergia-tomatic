3d30dd4602b613e6d0375b06970cc94d8c57d8bc
David García Garzón <voki@canvoki.net>
2020-06-15 14:49:28 +0200

    warnturn: warn to each person

--- crontab-warnturn.sh
+++ crontab-warnturn.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-./tomatic_says.py "Canvi de torn en 5m: $(tomatic_rtqueue.py preview --time $(date -d '10 minutes' +'%H:%M') )"
+turn=$(tomatic_rtqueue.py preview --time $(date -d '10 minutes' +'%H:%M') )
+./tomatic_says.py "Canvi de torn en 5m: $turn"
 ./tomatic_says.py "Recordeu anotar les trucades: https://intranet.helpscoutdocs.com/article/749-formulari-telefon-inquietuds-clients"
 
+for a in $turn
+do
+	./tomatic_says.py -c $turn "Hola super, tens torn de telefon en 5 minuts!"
+done
 
