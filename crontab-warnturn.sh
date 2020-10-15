bfc9a2c5a4a8949720e40d0368c1d770da110bc2
Som Energia IT <tomatic@somenergia.coop>
2020-10-15 10:32:52 +0200

    tomatic_says: elliot workarround in a wrapper

--- crontab-warnturn.sh
+++ crontab-warnturn.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+# To setup those task you should:
+#    sudo chown root:root crontab
+#    sudo chmod 755 crontab
 
 turn=$(tomatic_rtqueue.py preview --time $(date -d '10 minutes' +'%H:%M') )
-./tomatic_says.py "Canvi de torn en 5m: $turn"
-./tomatic_says.py "Recordeu anotar les trucades: https://intranet.helpscoutdocs.com/article/749-formulari-telefon-inquietuds-clients"
+
+./tomatic_says.sh "Canvi de torn en 5m: $turn"
+./tomatic_says.sh "Recordeu anotar les trucades: https://intranet.helpscoutdocs.com/article/749-formulari-telefon-inquietuds-clients"
 
 for person in $turn
 do
-	./tomatic_says.py -c ${person/,/} "Hola super, tens torn de telefon en 5 minuts!"
+	./tomatic_says.sh -c ${person/,/} "Hola super, tens torn de telefon en 5 minuts!"
 done
 
