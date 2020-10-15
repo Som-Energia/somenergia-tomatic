d7cf1cecb6f8834dae22f773ca7e2c462208ca3f
Som Energia IT <tomatic@somenergia.coop>
2020-10-15 15:44:48 +0200

    crontab-dailyreport: no call reports

--- crontab-dailyreport.sh
+++ crontab-dailyreport.sh
@@ -4,8 +4,8 @@ export PYTHONIOENCODING=utf8
 
 missatge="$(
     echo "Ei Supers! Ja hem tancat telèfons!"
-    ./tomatic_calls.py summary
     echo "Gràcies per la feina feta!"
+#  ./tomatic_calls.py summary
 )"
 ./tomatic_says.sh "$missatge"
 
