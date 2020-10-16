854e7b1cd853b3d9150336d37f020bb0b838531f
David García Garzón <voki@canvoki.net>
2020-10-16 12:00:58 +0200

    persons: centralizing how to obtain person data

---
+++ tomatic/persons.py
@@ -0,0 +1,58 @@
+from yamlns import namespace as ns
+from pathlib2 import Path
+
+srcpath = Path(__file__).absolute().parent
+
+def persons(path=None):
+    """
+    Cached load of personal information.
+    First pathless call loads from default path.
+    Explicit path reloads.
+    Further pathless calls, just uses cache.
+    If modifications are detected for the path, it is reloaded.
+    False as path resets the cache.
+    """
+    if path is False:
+        if hasattr(persons,'cache'):
+            del persons.cache
+        return
+    if not hasattr(persons,'cache'):
+        persons.path = Path(path) if path else (srcpath / '../persons.yaml').resolve()
+        persons.mtime = persons.path.stat().st_mtime
+        persons.cache = ns.load(persons.path)
+        return persons.cache
+
+    if path:
+        persons.path = Path(path)
+        persons.mtime = persons.path.stat().st_mtime
+        persons.cache = ns.load(persons.path)
+        return persons.cache
+
+    if persons.mtime != persons.path.stat().st_mtime:
+        persons.mtime = persons.path.stat().st_mtime
+        persons.cache = ns.load(persons.path)
+
+    return persons.cache
+
+def byExtension(extension):
+    """
+    Returns the person key having the extension
+    """
+    keytoext = dict(
+        (e,n) for n,e in persons().extensions.items()
+    )
+    return keytoext.get(extension,extension)
+
+def name(key):
+    """
+    Returns the person full name having the person key
+    """
+    return persons().names.get(key,key.title())
+
+def nameByExtension(extension):
+    return name(byExtension(extension))
+
+
+
+
+# vim: et ts=4 sw=4
---
+++ tomatic/persons_test.py
@@ -0,0 +1,115 @@
+import unittest
+import persons
+from pathlib2 import Path
+from yamlns import namespace as ns
+
+class Persons_Test(unittest.TestCase):
+
+    from yamlns.testutils import assertNsEqual
+
+    def setUp(self):
+        ''
+
+
+    def tearDown(self):
+        persons.persons(False)
+
+    def test_persons_explicitPath(self):
+        Path('p.yaml').write_text(u"hola: tu")
+        ps = persons.persons('p.yaml')
+        self.assertNsEqual(ps, """\
+            hola: tu
+        """)
+        self.assertEqual(persons.persons.path, Path('p.yaml'))
+
+    def test_persons_defaultPath(self):
+        ps = persons.persons()
+        self.assertEqual(persons.persons.path, 
+            (Path(__file__) / '../../persons.yaml').resolve()
+        )
+
+    def test_persons_cached(self):
+        Path('p.yaml').write_text(u"hola: tu")
+        persons.persons('p.yaml')
+        persons.persons.cache = ns.loads("ni: yo")
+        ps = persons.persons()
+        self.assertNsEqual(ps, """\
+            ni: yo
+        """)
+
+    def test_persons_falseInvalidatesCache(self):
+        Path('p.yaml').write_text(u"hola: tu")
+        persons.persons('p.yaml')
+        persons.persons.cache = ns.loads("ni: yo")
+        ps = persons.persons(False)
+        self.assertEqual(ps, None)
+        ps = persons.persons('p.yaml')
+        self.assertNsEqual(ps, """\
+            hola: tu
+        """)
+
+    def test_persons_updated(self):
+        Path('p.yaml').write_text(u"hola: tu")
+        persons.persons('p.yaml')
+        import time
+        time.sleep(1) # TODO: use ns 
+        Path('p.yaml').write_text(u"hola: you")
+        ps = persons.persons()
+        self.assertNsEqual(ps, """\
+            hola: you
+        """)
+
+    def test_persons_differentPath(self):
+        Path('p.yaml').write_text(u"hola: tu")
+        Path('p2.yaml').write_text(u"hola: yo")
+        persons.persons('p.yaml')
+        ps = persons.persons('p2.yaml')
+        self.assertNsEqual(ps, """\
+            hola: yo
+        """)
+
+    def test_byExtension(self):
+        Path('p.yaml').write_text(u"""\
+            extensions:
+                me: "6666"
+            """)
+        persons.persons('p.yaml')
+        self.assertEqual(
+            persons.byExtension('6666'),
+            'me')
+        self.assertEqual(
+            persons.byExtension('1111'),
+            '1111')
+
+    def test_name(self):
+        Path('p.yaml').write_text(u"""\
+            names:
+                jm: Jose Miguel
+            """)
+
+        persons.persons('p.yaml')
+        self.assertEqual(
+            persons.name('jm'),
+            'Jose Miguel')
+
+        self.assertEqual(
+            persons.name('missing'),
+            'Missing')
+        
+    def test_nameByExtension(self):
+        Path('p.yaml').write_text(u"""\
+            extensions:
+                jm: "6666"
+            names:
+                jm: Jose Miguel
+            """)
+
+        persons.persons('p.yaml')
+        self.assertEqual(
+            persons.nameByExtension('6666'),
+            'Jose Miguel')
+
+        
+
+
+# vim: et ts=4 sw=4
