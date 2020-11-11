d354efe628e77c0dc891a73555ee85192b0c9d92
pol <pol.monso@somenergia.coop>
2020-11-11 13:13:18 +0100

    default extension argument when empty

--- tomatic/api.py
+++ tomatic/api.py
@@ -687,8 +687,9 @@ def getPhoneLog(phone):
     return yamlfy(info=result)
 
 
+@app.route('/api/personlog/', methods=['GET'])
 @app.route('/api/personlog/<ext>', methods=['GET'])
-def getMyLog(ext):
+def getMyLog(ext=-1):
     message = 'ok'
     mylog = ""
     try:
