4928172601dd6f063787fe8610238126da9033d9
David García Garzón <david.garcia@somenergia.coop>
2020-12-03 21:52:57 +0100

    asteriskfake: the new iface for pbx mockup

---
+++ tomatic/asteriskfake.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+from yamlns import namespace as ns
+from . import persons
+
+class AsteriskFake(object):
+    ''
+    def __init__(self):
+        self._queuepaused = dict()
+        self._queueagents = dict()
+        self._extensions = list()
+
+    def _agents(self, queue):
+        return self._queueagents.setdefault(queue, list())
+
+    def _paused(self, queue):
+        return self._queuepaused.setdefault(queue, set())
+
+    def setQueue(self, queue, persons):
+        """Sets persons as current agents of the queue, removing previous ones"""
+        self._queueagents[queue] = list()
+        self._queuepaused[queue] = set()
+        for person in persons:
+            self.add(queue, person)
+
+    def add(self, queue, name):
+        """Add person as next agent of the queue"""
+
+        extension = persons.extension(name)
+        if not extension: return
+        self._agents(queue).append(name)
+
+    def queue(self, queue):
+        """Returns information of the current agents in the queue"""
+        return [
+            ns(
+                key = who,
+                paused = who in self._paused(queue),
+                extension = persons.extension(who),
+            )
+            for who in self._agents(queue)
+        ]
+
+    def pause(self, queue, who):
+        """Temporary removes the agent from the queue"""
+        self._paused(queue).add(who)
+
+    def resume(self, queue, who):
+        """Puts back the agent to the queue"""
+        if who in self._paused(queue):
+            self._paused(queue).remove(who)
+
+    def addExtension(self, extension, fullname):
+        """Setup the extension to be used by fullname"""
+        self._extensions.append(ns(
+            extension=extension,
+            fullname=fullname,
+        ))
+
+    def removeExtension(self, extension):
+        """Deallocates the extension"""
+        self._extensions = [
+            ext for ext in self._extensions
+            if ext.extension != extension
+            ]
+
+    def clearExtensions(self):
+        """Deallocates all the extensions"""
+        self._extensions = list()
+
+    def extensions(self):
+        """Returns the setup extensions"""
+        return [(
+            #persons.byExtension(info.extension),
+            info.extension,
+            u"{fullname} <{extension}>".format(**info),
+            )
+            for info in self._extensions
+        ]
+
+
+# vim: ts=4 sw=4 et
---
+++ tomatic/asteriskfake_test.py
@@ -0,0 +1,242 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from pathlib2 import Path
+from . import persons
+from .asteriskfake import AsteriskFake
+
+
+class AsteriskFake_Test(unittest.TestCase):
+
+    from yamlns.testutils import assertNsEqual
+    def setUp(self):
+        self.a = AsteriskFake()
+
+        self.persons = Path('p.yaml')
+
+        self.persons.write_text(u"""\
+            extensions:
+              alice: '200'
+              bob: '202'
+              carol: '204'
+              diana: '400'
+              eva: '402'
+              fanny: '404'
+        """)
+        ps = persons.persons(self.persons)
+
+    def tearDown(self):
+        persons.persons(False) # reset
+        self.persons.unlink()
+
+    def fixture(self):
+        return self.a
+
+    def assertNames(self, result, expected):
+        self.assertEqual([
+            member.key
+            for member in result
+            ], expected)
+
+    def assertPaused(self, result, expected):
+        self.assertEqual([
+            (member.key, member.paused)
+            for member in result
+            ], expected)
+
+    def assertExtensions(self, result, expected):
+        self.assertEqual([
+            (member.key, member.extension)
+            for member in result
+            ], expected)
+
+    def test_setQueue(self):
+        a = self.fixture()
+        a.setQueue('aqueue',['alice', 'carol', 'bob' ])
+        self.assertNames(a.queue('aqueue'), [
+            'alice',
+            'carol',
+            'bob',
+        ])
+
+    def test_queue_extensions(self):
+        a = self.fixture()
+        a.setQueue('aqueue',['alice', 'carol', 'bob' ])
+        self.assertExtensions(a.queue('aqueue'), [
+            ('alice', '200'),
+            ('carol', '204'),
+            ('bob', '202'),
+        ])
+
+    def test_setQueue_ignoresOutsiders(self):
+        a = self.fixture()
+        a.setQueue('aqueue',['alice', 'carol', 'intruder' ])
+        self.assertNames(a.queue('aqueue'), [
+            'alice',
+            'carol',
+        ])
+
+
+    def test_setQueue_overwrites(self):
+        a = self.fixture()
+        a.setQueue('aqueue',['alice'])
+        a.setQueue('aqueue',['carol', 'bob' ])
+        self.assertNames(a.queue('aqueue'), [
+            'carol',
+            'bob',
+        ])
+
+    def test_setQueue_emptyClears(self):
+        a = self.fixture()
+        a.setQueue('aqueue',['alice', 'carol', 'bob' ])
+        a.setQueue('aqueue',[])
+        self.assertNames(a.queue('aqueue'), [
+        ])
+
+    def test_add(self):
+        a = self.fixture()
+        a.setQueue('aqueue',['alice', 'carol'])
+        a.add('aqueue', 'diana')
+        self.assertNames(a.queue('aqueue'), [
+            'alice',
+            'carol',
+            'diana',
+        ])
+
+    def test_add_ignoresIntruder(self):
+        a = self.fixture()
+        a.setQueue('aqueue',['alice', 'carol'])
+        a.add('aqueue', 'intruder')
+        self.assertNames(a.queue('aqueue'), [
+            'alice',
+            'carol',
+        ])
+
+    def test_pause_defaultFalse(self):
+        a = self.fixture()
+        a.setQueue('aqueue',['alice', 'carol', 'bob' ])
+        self.assertPaused(a.queue('aqueue'), [
+            ('alice', False),
+            ('carol', False),
+            ('bob', False),
+        ])
+ 
+    def test_pause_anActiveMember(self):
+        a = self.fixture()
+        a.setQueue('aqueue',['alice', 'carol', 'bob' ])
+        a.pause('aqueue','carol')
+        self.assertPaused(a.queue('aqueue'), [
+            ('alice', False),
+            ('carol', True),
+            ('bob', False),
+        ])
+        
+    def test_pause_notInQueue_ignored(self):
+        a = self.fixture()
+        a.setQueue('aqueue',['alice', 'carol' ])
+        a.pause('aqueue','bob')
+        self.assertPaused(a.queue('aqueue'), [
+            ('alice', False),
+            ('carol', False),
+        ])
+
+    def test_pause_intruder_ignored(self):
+        a = self.fixture()
+        a.setQueue('aqueue',['alice', 'carol', 'bob' ])
+        a.pause('aqueue','intruder')
+        self.assertPaused(a.queue('aqueue'), [
+            ('alice', False),
+            ('carol', False),
+            ('bob', False),
+        ])
+
+    def test_resume(self):
+        a = self.fixture()
+        a.setQueue('aqueue',['alice', 'carol', 'bob' ])
+        a.pause('aqueue','carol')
+        a.resume('aqueue','carol')
+        self.assertPaused(a.queue('aqueue'), [
+            ('alice', False),
+            ('carol', False),
+            ('bob', False),
+        ])
+
+    def test_setQueue_leavesOtherQueuesAlone(self):
+        a = self.fixture()
+        a.setQueue('aqueue',['alice', 'carol', 'bob' ])
+        a.setQueue('otherqueue',['diana', 'fanny', 'eva' ])
+        self.assertNames(a.queue('aqueue'), [
+            'alice',
+            'carol',
+            'bob',
+        ])
+
+    def test_pause_leavesOtherQueuesAlone(self):
+        a = self.fixture()
+        a.setQueue('aqueue',['alice'])
+        a.setQueue('otherqueue',['alice' ])
+        a.pause('otherqueue', 'alice')
+        self.assertPaused(a.queue('aqueue'), [
+            ('alice', False),
+        ])
+        self.assertPaused(a.queue('otherqueue'), [
+            ('alice', True),
+        ])
+
+    def test_resume_leavesOtherQueuesAlone(self):
+        a = self.fixture()
+        a.setQueue('aqueue',['alice'])
+        a.setQueue('otherqueue',['alice' ])
+        a.pause('aqueue', 'alice')
+        a.pause('otherqueue', 'alice')
+        a.resume('otherqueue', 'alice')
+        self.assertPaused(a.queue('aqueue'), [
+            ('alice', True),
+        ])
+        self.assertPaused(a.queue('otherqueue'), [
+            ('alice', False),
+        ])
+
+    # Extensions
+
+    def test_extensions_empty(self):
+        a = self.fixture()
+        self.assertEqual(a.extensions(), [
+        ])
+ 
+    def test_extensions_withOne(self):
+        a = self.fixture()
+        a.addExtension('201', 'Perico Palotes')
+        self.assertEqual(a.extensions(), [
+            ('201','Perico Palotes <201>'),
+        ])
+ 
+    def test_extensions_withMany(self):
+        a = self.fixture()
+        a.addExtension('201', 'Perico Palotes')
+        a.addExtension('202', 'Juana Calamidad')
+        self.assertEqual(a.extensions(), [
+            ('201','Perico Palotes <201>'),
+            ('202','Juana Calamidad <202>'),
+        ])
+ 
+
+    def test_clearExtensions(self):
+        a = self.fixture()
+        a.addExtension('201', 'Perico Palotes')
+        a.clearExtensions()
+        self.assertEqual(a.extensions(), [
+        ])
+ 
+    def test_removeExtensions(self):
+        a = self.fixture()
+        a.addExtension('201', 'Perico Palotes')
+        a.addExtension('202', 'Juana Calamidad')
+        a.removeExtension('201')
+        self.assertEqual(a.extensions(), [
+            ('202','Juana Calamidad <202>'),
+        ])
+ 
+
+
+# vim: ts=4 sw=4 et
