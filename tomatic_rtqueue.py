8106ac7b4443a4cfb414cc7f8e0ca79212f8cbc3
David García Garzón <david.garcia@somenergia.coop>
2020-12-03 20:48:56 +0100

    rtqueue parse coded all into pbxasterisk

--- tomatic_rtqueue.py
+++ tomatic_rtqueue.py
@@ -159,41 +159,6 @@ def status(queue, yaml=False):
 
 
 
-from tomatic.pbxasterisk import extract, extractQueuepeerInfo
-
-def extractQueueInfo(output):
-	return [
-		extractQueuepeerInfo(line)
-		for line in output.splitlines()
-		if line.strip().startswith('SIP/')
-	]
-
-def lala():
-	if fake:
-		output = u"""\
-somenergia has 0 calls (max unlimited) in 'leastrecent' strategy (4s holdtime, 340s talktime), W:0, C:159, A:88, SL:100.0% within 30s
-   Members: 
-	  SIP/3063@bustia_veu (SIP/3063) (ringinuse disabled) (realtime) (Not in use) (paused) has taken 6 calls (last was 181 secs ago)
-	  SIP/3188@bustia_veu (SIP/3188) (ringinuse disabled) (realtime) (in call) (In use) has taken 3 calls (last was 630 secs ago)
-	  SIP/3043@bustia_veu (SIP/3043) (ringinuse disabled) (realtime) (Not in use) has taken 4 calls (last was 257 secs ago)
-	  SIP/3084@bustia_veu (SIP/3084) (ringinuse disabled) (realtime) (Not in use) has taken 6 calls (last was 187 secs ago)
-	  SIP/2905@bustia_veu (SIP/2905) (ringinuse disabled) (realtime) (Ringing) (In use) has taken 5 calls (last was 564 secs ago)
-	  SIP/3048@bustia_veu (SIP/3048) (ringinuse disabled) (realtime) (Not in use) has taken 4 calls (last was 189 secs ago)
-	  SIP/2902@bustia_veu (SIP/2902) (ringinuse disabled) (realtime) (in call) (In use) has taken 2 calls (last was 1367 secs ago)
-   No Callers
-"""
-	else:
-		remote = Remote(**dbconfig.tomatic.ssh)
-		output = remote.run("asterisk -rx 'queue show {}'".format(queue))
-
-	if raw:
-		click.echo(output)
-	if sql:
-		sortida = remote.run('''echo 'select callerid, paused, sippeers.* from queue_members join sippeers on queue_members.interface = concat("SIP/", sippeers.name);' | sudo mysql asterisk''')
-		click.echo(sortida)
-
-
-
 
 
 if __name__=='__main__':
