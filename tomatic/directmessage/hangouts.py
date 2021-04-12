1e0fbfe7736b34eed4a1379121b3947a88ec95e5
David García Garzón <david.garcia@somenergia.coop>
2021-04-12 14:29:28 +0200

    hangouts: removed main in cli tomatic_says.py

--- tomatic/directmessage/hangouts.py
+++ tomatic/directmessage/hangouts.py
@@ -152,26 +152,4 @@ def send(channel, message, tokenfile=None):
     loop.run_until_complete(client.connect())
 
 
-if __name__ == '__main__':
-    import click
-
-    @click.command()
-    @click.argument('message', nargs=-1)
-    @click.option('-c', '--channel',
-        help='message target: a hangout channel id, email or tomatic user',
-        )
-    @click.option('-t', '--tokenfile',
-        help='Token file instead the default one',
-        )
-    def main(channel, message, tokenfile):
-        if not message:
-            message = sys.stdin.read()
-        else:
-            message=' '.join(message)
-        step("Sending: '{}'", message)
-
-        send(channel, message, tokenfile)
-
-    main()
-
 # vim: et ts=4 sw=4
