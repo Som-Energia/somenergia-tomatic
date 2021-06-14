8855dedc9c0951fc1e5094a1987412a2fd3765b0
David García Garzón <david.garcia@somenergia.coop>
2021-06-14 20:02:35 +0200

    extracted CallRegistry to its onw file

--- tomatic/api.py
+++ tomatic/api.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from future import standard_library
-standard_library.install_aliases()
 from flask import (
     Flask, Response, request,
     send_from_directory,
@@ -9,21 +7,19 @@ from flask import (
     )
 from datetime import datetime, timedelta, timezone
 from threading import Semaphore
-import os.path
 import urllib.parse
 import decorator
 import erppeek
 from pathlib2 import Path
-from sheetfetcher import SheetFetcher
 from yamlns import namespace as ns
 from consolemsg import error, step, warn, u
 
 from .callinfo import CallInfo
+from .callregistry import CallRegistry
 from . import schedulestorage
 from .asteriskfake import AsteriskFake
 from .pbxqueue import PbxQueue
 from . import persons
-from .claims import Claims
 
 try:
     import dbconfig
@@ -499,87 +495,6 @@ def getConnectionInfo():
     )
     return yamlfy(info=result)
 
-
-class CallRegistry(object):
-    def __init__(self, path=None, size=20):
-        self.path = Path(path or CONFIG.my_calls_log)
-        self.path.parent.mkdir(parents=True, exist_ok=True)
-        self.size = size
-
-    def _calls(self):
-        if not self.path.exists():
-            return ns()
-        return ns.load(self.path)
-
-    def callsByExtension(self, extension):
-        return self._calls().get(extension,[])
-
-    def updateCall(self, extension, fields):
-        calls = self._calls()
-        extensionCalls = calls.setdefault(extension,[])
-        for call in extensionCalls:
-            if call.data == fields.data:
-                call.update(fields)
-                break
-        else: # exiting when not found
-            extensionCalls.append(fields)
-
-        if self.size:
-            extensionCalls=extensionCalls[-self.size:]
-
-        calls.dump(self.path)
-
-    def annotateInfoRequest(self, data):
-        self._appendToExtensionDailyInfo('info_cases', data)
-
-    def annotateClaim(self, data):
-        self._appendToExtensionDailyInfo('atc_cases', data)
-
-    def _appendToExtensionDailyInfo(self, prefix, info, date=datetime.today()):
-        path = self.path.parent / prefix / '{:%Y%m%d}.yaml'.format(date)
-        warn("Saving {}", path)
-        dailyInfo = ns()
-        if path.exists():
-            dailyInfo = ns.load(str(path))
-        dailyInfo.setdefault(info.person, []).append(info)
-        path.parent.mkdir(parents=True, exist_ok=True)
-        dailyInfo.dump(str(path))
-
-    def infoRequestTypes(self):
-        try:
-            content = Path(CONFIG.info_cases).read_text(encoding='utf8')
-        except Exception as e:
-            error("Error carregant el tipus d'anotacions de trucades: {}", e)
-            return []
-        return [
-            line.strip()
-            for line in content.splitlines()
-            if line.strip()
-        ]
-
-    def claimTypes(self):
-        try:
-            content = Path(CONFIG.claims_file).read_text(encoding='utf8')
-        except Exception as e:
-            error("Error carregant el tipus d'anotacions de trucades: {}", e)
-            return []
-        return [
-            line.strip()
-            for line in content.splitlines()
-            if line.strip()
-        ]
-
-    def importClaimTypes(self, erp):
-        claims = Claims(erp)
-        erp_claims = claims.get_claims()
-
-        Path(CONFIG.claims_file).write_text(
-            '\n'.join([u(x) for x in erp_claims]),
-            encoding='utf8',
-        )
-
-    def claimKeywords(self):
-        return ns.load(CONFIG.claims_dict_file)
 
 @app.route('/api/personlog/<ext>', methods=['GET'])
 def getCallLog(ext):
---
+++ tomatic/callregistry.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+from pathlib2 import Path
+from yamlns import namespace as ns
+from consolemsg import error, step, warn, u
+from .claims import Claims
+
+def fillConfigurationInfo():
+    return ns.load('config.yaml')
+
+CONFIG = fillConfigurationInfo()
+
+class CallRegistry(object):
+    def __init__(self, path=None, size=20):
+        self.path = Path(path or CONFIG.my_calls_log)
+        self.path.parent.mkdir(parents=True, exist_ok=True)
+        self.size = size
+
+    def _calls(self):
+        if not self.path.exists():
+            return ns()
+        return ns.load(self.path)
+
+    def callsByExtension(self, extension):
+        return self._calls().get(extension,[])
+
+    def updateCall(self, extension, fields):
+        calls = self._calls()
+        extensionCalls = calls.setdefault(extension,[])
+        for call in extensionCalls:
+            if call.data == fields.data:
+                call.update(fields)
+                break
+        else: # exiting when not found
+            extensionCalls.append(fields)
+
+        if self.size:
+            extensionCalls=extensionCalls[-self.size:]
+
+        calls.dump(self.path)
+
+    def annotateInfoRequest(self, data):
+        self._appendToExtensionDailyInfo('info_cases', data)
+
+    def annotateClaim(self, data):
+        self._appendToExtensionDailyInfo('atc_cases', data)
+
+    def _appendToExtensionDailyInfo(self, prefix, info, date=datetime.today()):
+        path = self.path.parent / prefix / '{:%Y%m%d}.yaml'.format(date)
+        warn("Saving {}", path)
+        dailyInfo = ns()
+        if path.exists():
+            dailyInfo = ns.load(str(path))
+        dailyInfo.setdefault(info.person, []).append(info)
+        path.parent.mkdir(parents=True, exist_ok=True)
+        dailyInfo.dump(str(path))
+
+    def infoRequestTypes(self):
+        try:
+            content = Path(CONFIG.info_cases).read_text(encoding='utf8')
+        except Exception as e:
+            error("Error carregant el tipus d'anotacions de trucades: {}", e)
+            return []
+        return [
+            line.strip()
+            for line in content.splitlines()
+            if line.strip()
+        ]
+
+    def claimTypes(self):
+        try:
+            content = Path(CONFIG.claims_file).read_text(encoding='utf8')
+        except Exception as e:
+            error("Error carregant el tipus d'anotacions de trucades: {}", e)
+            return []
+        return [
+            line.strip()
+            for line in content.splitlines()
+            if line.strip()
+        ]
+
+    def importClaimTypes(self, erp):
+        claims = Claims(erp)
+        erp_claims = claims.get_claims()
+
+        Path(CONFIG.claims_file).write_text(
+            '\n'.join([u(x) for x in erp_claims]),
+            encoding='utf8',
+        )
+
+    def claimKeywords(self):
+        return ns.load(CONFIG.claims_dict_file)
+
+# vim: ts=4 sw=4 et
