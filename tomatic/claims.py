0d5644376b431833b222e6e3a9a47fc27c3aa22c
David García Garzón <david.garcia@somenergia.coop>
2021-11-12 17:19:51 +0100

    crm_categories: single erp call speed up

--- tomatic/claims.py
+++ tomatic/claims.py
@@ -96,19 +96,12 @@ class Claims(object):
         return claims
 
     def crm_categories(self):
-        crm_categories_model = self.erp.model('crm.case.categ')
-        all_crm_categories = crm_categories_model.search([])
-
-        crm_categories = []
-        for category_id in all_crm_categories:
-            category = crm_categories_model.read(
-                category_id, ['name']
-            )
-            categ_name = category.get('name')
-            if categ_name.startswith('['):
-                crm_categories.append(categ_name)
-
-        return crm_categories
+        ids = self.erp.CrmCaseCateg.search([])
+        return [
+            category['name']
+            for category in self.erp.CrmCaseCateg.read(ids,['name'])
+            if category['name'].startswith('[')
+        ]
 
     def create_crm_case(self, case):
         ''
