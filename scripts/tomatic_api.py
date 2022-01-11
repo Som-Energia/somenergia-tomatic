6f1e688b52446a198640f66f672051764bd2e992
David García Garzón <david.garcia@somenergia.coop>
2022-01-11 02:28:56 +0100

    removing int cast in queue param in api cli

--- scripts/tomatic_api.py
+++ scripts/tomatic_api.py
@@ -44,7 +44,6 @@ def now(date, time):
     help="Override pbx backend configured in configuration",
     )
 @click.option('--queue', '-q',
-    type=int,
     default=None,
     help="Override pbx queue configured in configuration",
     )
