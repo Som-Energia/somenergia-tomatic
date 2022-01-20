9f28565d7623452fa746a4a744ae734b275d2c76
David García Garzón <david.garcia@somenergia.coop>
2022-01-20 22:31:12 +0100

    upgrade to webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -3,32 +3,48 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
 
 var config = {
 	context: path.resolve(__dirname, 'tomatic/static/'),
 	entry: {
 		main: './graella.js',
 	},
+	resolve: {
+		fallback: {
+			buffer: require.resolve('buffer'),
+		},
+	},
 	output: {
 		path: path.resolve(__dirname, 'tomatic/dist'),
-		filename: '[name]-bundle-[chunkhash].js',
+		filename: '[name]-bundle-[contenthash].js',
 		chunkFilename: '[id].chunk.js',
+	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					name: 'vendor',
+					chunks: 'all',
+					test: /[\\/]node_modules[\\/]/,
+				},
+			},
+		},
 	},
 	module: {
 		rules: [
-			{ test: /\.yaml$/,   loader: "yaml-loader" },
-			{ test: /\.css$/,    use: ExtractTextPlugin.extract({use: "css-loader"})},
-			{ test: /\.less$/,   use: ExtractTextPlugin.extract({use: "css-loader!less-loader"})},
-			{ test: /\.styl$/,   use: ExtractTextPlugin.extract({use: "css-loader!stylus-loader"})},
-			{ test: /\.jade$/,   loader: "jade-loader?self" },
-			{ test: /\.png$/,    loader: "url-loader?prefix=img/&limit=5000" },
-			{ test: /\.jpg$/,    loader: "url-loader?prefix=img/&limit=5000" },
-			{ test: /\.gif$/,    loader: "url-loader?prefix=img/&limit=5000" },
-			{ test: /\.woff$/,   loader: "file-loader?prefix=font/&limit=5000" },
-			{ test: /\.eot$/,    loader: "file-loader?prefix=font/" },
-			{ test: /\.ttf$/,    loader: "file-loader?prefix=font/" },
-			{ test: /\.svg$/,    loader: "file-loader?prefix=font/" },
+			{ test: /\.yaml$/,  loader: "yaml-loader" },
+			{ test: /\.css$/,   use: [MiniCssExtractPlugin.loader, "css-loader"]},
+			{ test: /\.less$/,  use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"]},
+			{ test: /\.styl$/,  use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"]},
+			{ test: /\.(gif|jpg|png)$/, type: "asset", generator: {
+				filename: "[name]-[hash][ext]"
+			}},
+			{ test: /\.(woff|eot|ttf|svg)$/, type: "asset", generator: {
+				filename: "font/[name][ext]"
+			}},
 		]
 	},
 	plugins:[
@@ -36,24 +52,17 @@ var config = {
 		new HtmlWebpackPlugin({
 			template: './tomatic.html',
 			favicon: './favicon.ico',
-			inject:'header',
+			inject: 'head',
 		}),
-		// Uglifies JS
-//		new webpack.optimize.UglifyJsPlugin(),
+		/*
 		// Analyzes generated sizes
-//		new BundleAnalyzerPlugin({ analyzerMode: 'static' }),
-		// Split vendor stuff from project's
-		new webpack.optimize.CommonsChunkPlugin({
-			name: "vendor",
-			minChunks: function(module, num) {
-				return /node_modules/.test(module.context);
-			}
-		}),
-		// Extract webpack common stub from vendor and project
-		new webpack.optimize.CommonsChunkPlugin({ name: "manifest", }),
-		// Split css included as js into a separate file again
-		new ExtractTextPlugin("styles-[chunkhash].css"),
+		new BundleAnalyzerPlugin({ analyzerMode: 'static' }),
+		*/
 		new CleanWebpackPlugin('tomatic/dist/*'),
+		// Split css included as js into a separate file again
+		new MiniCssExtractPlugin({
+			filename: "styles-[chunkhash].css"
+		}),
 	]
 	
 };
