4152f69b6f8e5eede999d807b409e016681e826c
David García Garzón <david.garcia@somenergia.coop>
2022-04-06 07:16:13 +0200

    dump stats

--- scripts/areavoip_dumpstats.sh
+++ scripts/areavoip_dumpstats.sh
@@ -12,6 +12,7 @@ do
 	areavoip_callapi.py INFO info simplecdrs start "$date 00:00:00" end "$date 23:59:59" format csv  secondsTimeout 20 > "dump_stats/simple_cdrs-$date.csv"
 	areavoip_callapi.py INFO info cdrs start "$date 00:00:00" end "$date 23:59:59" format json secondsTimeout 20 > "dump_stats/cdrs-$date.yaml"
 	areavoip_callapi.py INFO info cdrs start "$date 00:00:00" end "$date 23:59:59" format csv  secondsTimeout 20 > "dump_stats/cdrs-$date.csv"
+	tomatic_dailyreport.py stats --backend areavoip --date $date
 	[[ "$date" < "$last" ]] || exit
 	date=$(date -I -d "$date+1day")
 done
