ae879c3aad055818978fd3bd7db5a143759e2336
David García Garzón <david.garcia@somenergia.coop>
2022-07-18 08:31:03 +0200

    select: spaces -> tabs

--- tomatic/ui/components/select.js
+++ tomatic/ui/components/select.js
@@ -1,51 +1,51 @@
 'use strict';
 module.exports = function() {
 
-    var m = require('mithril');
+	var m = require('mithril');
 
-    m.prop = require('mithril/stream');
+	m.prop = require('mithril/stream');
 
-    var Select = {
-        oninit: function(vnode) {
-            vnode.state.value = m.prop(vnode.attrs.value);
-            vnode.state.hasFocus = false;
-            vnode.state.setFocus = function setFocus(hasFocus) {
-                console.log("Getting focus:", hasFocus);
-                vnode.state.hasFocus = hasFocus;
-            };
-        },
-        view: function(vnode) {
-            return m('.pe-textfield.pe-textfield--floating-label'
-                    +'.pe-textfield--hide-clear.pe-textfield--dirty'
-                    +(vnode.state.hasFocus?'.pe-textfield--focused':'') 
-                    +(vnode.attrs.required!==undefined?'.pe-textfield--required':'')
-                , [
-                m('.pe-textfield__input-area', [
-                    m('label.pe-textfield__label', vnode.attrs.label),
-                    m('select.pe-textfield__input', {
-                        value: vnode.state.value(),
-                        onchange: function(ev) {
-                            vnode.state.value(ev.target.value);
-                            vnode.attrs.onChange(ev);
-                        },
-                        onfocus: function(ev) {
-                            vnode.state.setFocus(true)
-                        },
-                        onblur: function(ev) {
-                            vnode.state.setFocus(false)
-                        },
-                    },
-                    Object.keys(vnode.attrs.options).map(function(value) {
-                        return m('option', {
-                            value: value,
-                        }, vnode.attrs.options[value]);
-                    })),
-                ]),
-            ]);
-        },
-    };
+	var Select = {
+		oninit: function(vnode) {
+			vnode.state.value = m.prop(vnode.attrs.value);
+			vnode.state.hasFocus = false;
+			vnode.state.setFocus = function setFocus(hasFocus) {
+				console.log("Getting focus:", hasFocus);
+				vnode.state.hasFocus = hasFocus;
+			};
+		},
+		view: function(vnode) {
+			return m('.pe-textfield.pe-textfield--floating-label'
+					+'.pe-textfield--hide-clear.pe-textfield--dirty'
+					+(vnode.state.hasFocus?'.pe-textfield--focused':'') 
+					+(vnode.attrs.required!==undefined?'.pe-textfield--required':'')
+				, [
+				m('.pe-textfield__input-area', [
+					m('label.pe-textfield__label', vnode.attrs.label),
+					m('select.pe-textfield__input', {
+						value: vnode.state.value(),
+						onchange: function(ev) {
+							vnode.state.value(ev.target.value);
+							vnode.attrs.onChange(ev);
+						},
+						onfocus: function(ev) {
+							vnode.state.setFocus(true)
+						},
+						onblur: function(ev) {
+							vnode.state.setFocus(false)
+						},
+					},
+					Object.keys(vnode.attrs.options).map(function(value) {
+						return m('option', {
+							value: value,
+						}, vnode.attrs.options[value]);
+					})),
+				]),
+			]);
+		},
+	};
 
-    return Select;
+	return Select;
 }();
 
-// vim: et ts=4 sw=4
+// vim: noet ts=4 sw=4
