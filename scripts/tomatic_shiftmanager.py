c0c4963aa89d74c12fa4fc62b16ec901184c7d15
David García Garzón <david.garcia@somenergia.coop>
2022-08-16 21:13:58 +0200

    mails sending and other changes, omnibus, sorry
    - Send email if execution fails
    - Function to call api, which handles http errors
      and translate data.
    - Fix: unfilledCell is Complete not Complete**d**
    - Do not stop if already stopped

--- scripts/tomatic_shiftmanager.py
+++ scripts/tomatic_shiftmanager.py
@@ -7,30 +7,64 @@ import yaml
 from emili import sendMail 
 from io import open
 from yamlns import namespace as ns
-
+import dbconfig
 
 execution_id = sys.argv[1]
 
 config = ns.load('config.yaml')
 
+template = """\
+No he pogut completar la graella per la setmana.
 
-statusuri = config.baseUrl + f"/api/planner/api/status/{execution_id}"
-stopuri = config.baseUrl + f"/api/planner/api/stop/{execution_id}"
-uploaduri = config.baseUrl + f"/api/planner/api/upload/{execution_id}"
-killuri = config.baseUrl + f"/api/planner/api/kill/{execution_id}"
-
-# TODO: maybe already stopped
-stop = ns.loads(requests.get(stopuri).content).get('ok')
-if not stop:
-    kill = requests.get(killuri)
-
-status = ns.loads(requests.get(statusuri).content)
+- Execucio: {status.name}
+- Compleció: {status.completedCells} / {status.totalCells}
+- Penalitzacions: {status.solutionCost}
+- Cel·la de bloqueig: {status.unfilledCell}
+- [Revisar la graella]({config.baseUrl}/api/planner/solution/{execution_id})
+- [Mirar la sortida]({config.baseUrl}/api/planner/status/{execution_id})
+"""
 
 
-if status.unfilledCell == "Completed":
-    response = requests.get(uploaduri)
+def api(uri):
+    response = requests.get(config.baseUrl + uri)
+    if response.status_code != 200:
+        raise Exception(
+            f"While fetching {config.baseUrl}{uri}\n"
+            f"{response.status_code}: {str(response.content, 'utf8')}"
+        )
+    return ns.loads(response.content)
+
+statusuri = f"/api/planner/api/status/{execution_id}"
+stopuri = f"/api/planner/api/stop/{execution_id}"
+uploaduri = f"/api/planner/api/upload/{execution_id}"
+killuri = f"/api/planner/api/kill/{execution_id}"
+
+status = api(statusuri)
+print(status.dump())
+if status.get('status') != 'Stopped':
+    stop = api(stopuri).get('ok')
+    if not stop:
+        kill = api(killuri)
+
+if status.unfilledCell == "Complete":
+    response = api(uploaduri)
+    if response.get('ok')!=True:
+        pass # TODO: ERROR
 else:
-    pass
-    #TO DO implement send email
-    # send email
+    sendMail(
+        sender=dbconfig.tomatic.dailystats.sender,
+        to=dbconfig.tomatic.dailystats.recipients,
+        subject="ERROR: Generant la graelle setmanal {execution_id}",
+        md=template.format(
+            execution_id=execution_id,
+            config=config,
+            status=status,
+        ),
+        config='dbconfig.py',
+        verbose=True,
+    )
+
+
+
+
 
