7bf50cacf4f736388773546309e373900efed28d
David García Garzón <david.garcia@somenergia.coop>
2022-08-18 14:25:09 +0200

    reportrun outside crontab

--- scripts/crontab-launchtimetable.sh
+++ scripts/crontab-launchtimetable.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+supportmail=$(python -c "import dbconfig; print(dbconfig.tomatic.supportmail)")
+appmail=$(python -c "import dbconfig; print(dbconfig.smtp.user)")
 weeksInAdvance=2
 week=$(date +%F -d "next Monday + $weeksInAdvance weeks")
-tomatic_timetablelauncher.py $week
+reportrun -C dbconfig.py -f $appmail -t "$supportmail" -s "Tomatic Timetable Laucher Failed" -- tomatic_timetablelauncher.py $week
 
