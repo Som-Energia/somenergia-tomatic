d8fd6b2c50f37d55d64bc4c1ab78defe4dd2d1b4
Benjami <benjami94@gmail.com>
2022-10-31 16:02:44 +0100

    Added first test for editSlot endpoint

--- tomatic/api_test.py
+++ tomatic/api_test.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import b2btest
+import copy
 import unittest
 from datetime import datetime
+from mock import patch
 
+from yamlns import namespace as ns
 from . import api
+from fastapi.testclient import TestClient
+
 
 def setNow(year,month,day,hour,minute):
     api.now=lambda:datetime(year,month,day,hour,minute)
@@ -12,10 +16,44 @@ def setNow(year,month,day,hour,minute):
 class Api_Test(unittest.TestCase):
 
     def setUp(self):
-        api.app.config['TESTING'] = True
-        self.app = api.app.test_client()
         self.maxDiff = None
-        self.b2bdatapath = "testdata"
+        self.client = TestClient(api.app)
+
+    @patch("tomatic.api.schedulestorage.Storage.load")
+    @patch("tomatic.api.schedulestorage.Storage.save")
+    def test_editSlot_baseCase(self, mocked_saver, mocked_loader):
+        # Given a time table
+        old_time_table = ns(
+            hours=['09:00', '10:15'],
+            turns=['L1', 'L2'],
+            timetable=ns(
+                dl=[
+                    ['vic', 'cire', 'carme'],
+                    ['vic', 'cire', 'carme'],
+                ]
+            )
+        )
+        mocked_loader.return_value = old_time_table
+        day = 'dl'
+        hour_index = 0
+        turn_index = 0
+        name = 'cire'
+
+        # When we change one person in time table
+        response = self.client.patch(
+            '/api/graella/2022-10-31/{}/{}/{}/{}'.format(
+                day, hour_index, turn_index, name
+            ),
+            json="vic"
+        )
+
+        # The request was success
+        self.assertEquals(response.status_code, 200)
+        # and API saves the time table with change applied
+        new_time_table = copy.deepcopy(old_time_table)
+        new_time_table.timetable[day][hour_index][turn_index] = name
+        mocked_saver.assert_called_with(new_time_table)
+
 
 if __name__ == "__main__":
 
