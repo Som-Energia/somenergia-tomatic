58af4ab909a157cb8ceac378b20e0177a41a0480
joana <joana.figueira@somenergia.coop>
2022-11-24 16:47:44 +0100

    WiP: personal chats need to be fixed

--- scripts/crontab-warnturn.sh
+++ scripts/crontab-warnturn.sh
@@ -9,8 +9,9 @@ tomatic_says.py "Canvi de torn en 5m: $turn"
 tomatic_says.py "Recordeu anotar les trucades http://tomatic.somenergia.lan/#!/Trucada."
 tomatic_says.py "Qualsevol dubte que tingueu, podeu consultar a la intranet: https://intranet.helpscoutdocs.com/"
 
-for person in $turn
-do
-	tomatic_says.py -c ${person/,/} "Hola super, tens torn de telefon en 5 minuts!"
-done
+#To Do: send personal chats
+#for person in $turn
+#do
+#	tomatic_says.py -c ${person/,/} "Hola super, tens torn de telefon en 5 minuts!"
+#done
 
