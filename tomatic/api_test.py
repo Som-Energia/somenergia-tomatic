23939179dbd36b977b1e80d62fd5f798f957eefe
David García Garzón <david.garcia@somenergia.coop>
2023-02-23 18:54:04 +0100

    api test rewritten for auth compat and fragility

--- tomatic/api_test.py
+++ tomatic/api_test.py
@@ -1,25 +1,49 @@
 # -*- coding: utf-8 -*-
 
 import copy
-import yaml
 import unittest
 from datetime import datetime
 from mock import patch
+import json
 
-from yaml.loader import SafeLoader
 from yamlns import namespace as ns
 from . import api
 from fastapi.testclient import TestClient
+from .auth import validatedUser
 
 
 def setNow(year,month,day,hour,minute):
     api.now=lambda:datetime(year,month,day,hour,minute)
 
+
+@patch.dict('os.environ', TOMATIC_AUTH_DUMMY='vic')
 class Api_Test(unittest.TestCase):
+    from yamlns.testutils import assertNsEqual
 
     def setUp(self):
         self.maxDiff = None
         self.client = TestClient(api.app)
+        api.app.dependency_overrides[validatedUser] = (
+            lambda: dict(username='vic', email='me@here.coop')
+        )
+
+    def tearDown(self):
+        del api.app.dependency_overrides[validatedUser]
+
+    def assertResponseEqual(self, response, data, status=200):
+        if type(data) == str:
+            data = ns.loads(data)
+
+        self.assertNsEqual(
+            ns(
+                yaml=ns.loads(response.text),
+                status=response.status_code,
+            ),
+            ns(
+                yaml=data,
+                status=status,
+            ),
+        )
 
     def getTimeTableObjectStructure(self, time_table):
         return ns(
@@ -30,6 +54,9 @@ class Api_Test(unittest.TestCase):
 
     @patch("tomatic.api.schedulestorage.Storage.load")
     @patch("tomatic.api.schedulestorage.Storage.save")
+    @patch("tomatic.api.schedulestorage.utcnow",
+        lambda: datetime(2023,2,23,18,11,32,386313)
+    )
     def test_editSlot_baseCase(self, mocked_saver, mocked_loader):
         # Given a time table
         old_time_table = self.getTimeTableObjectStructure({
@@ -44,23 +71,32 @@ class Api_Test(unittest.TestCase):
         turn_index = 0
         name = 'cire'
 
+        expected = copy.deepcopy(old_time_table)
+        expected.timetable[day][hour_index][turn_index] = name
+        expected.overload=ns(
+            vic= -1,
+            cire= 1,
+        )
+        expected.log = [
+            '2023-02-23 18:11:32.386313: vic ha canviat dl 09:00-10:15 L1 de vic a cire'
+        ]
+
         # When we change one person in time table
         response = self.client.patch(
             '/api/graella/2022-10-31/{}/{}/{}/{}'.format(
                 day, hour_index, turn_index, name
             ),
-            json="vic"
         )
 
+
         # The request was success
-        self.assertEquals(response.status_code, 200)
+        self.assertResponseEqual(response, expected)
         # and API saves the time table with change applied
-        new_time_table = copy.deepcopy(old_time_table)
-        new_time_table.timetable[day][hour_index][turn_index] = name
-        mocked_saver.assert_called_with(new_time_table)
+        mocked_saver.assert_called_with(expected)
 
     @patch("tomatic.api.schedulestorage.Storage.load")
     @patch("tomatic.api.schedulestorage.Storage.save")
+    @patch.dict('tomatic.schedulestorage.CONFIG', maxNingusPerTurn=2)
     def test_editSlot_ningusLimit(self, mocked_saver, mocked_loader):
         # Given a time table
         old_time_table = self.getTimeTableObjectStructure({
@@ -84,16 +120,10 @@ class Api_Test(unittest.TestCase):
         )
 
         # API returns 400 error
-        self.assertEquals(response.status_code, 400)
         # with a message of the error that occurred
-        yaml_response = response.text
-        content_response = yaml.load(yaml_response, Loader=SafeLoader)
-        self.assertEquals(
-            content_response,
-            {'error': 'Hi ha masses Ningu en aquest torn'}
-        )
-        # and time table wasn't saved
-        mocked_saver.assert_not_called()
+        self.assertResponseEqual(response, """
+            error: Hi ha masses Ningu en aquest torn
+        """, 400)
 
 
 if __name__ == "__main__":
