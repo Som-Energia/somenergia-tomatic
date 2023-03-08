c0f8b66c138c62111350cc352e896464bb04b651
David García Garzón <david.garcia@somenergia.coop>
2023-03-08 08:20:41 +0100

    timetable launch not using the api

--- scripts/tomatic_timetable.py
+++ scripts/tomatic_timetable.py
@@ -3,8 +3,11 @@
 import click
 from consolemsg import fail, step
 from datetime import datetime, timedelta
-from tomatic import schedulestorage
 from pathlib import Path
+from yamlns.dateutils import Date
+from yamlns import ns
+from tomatic import schedulestorage
+from tomatic.plannerexecution import PlannerExecution, nextMonday
 
 @click.group()
 def cli():
@@ -25,6 +28,116 @@ def retireold():
     type=Path,
 )
 def upload():
+    schedules = schedulestorage.Storage()
+    step("uploading {}".format(yaml))
+    graella = ns.load(yaml)
+    logmsg = (
+        "{}: {} ha pujat {} ".format(
+        datetime.now(),
+        "tomatic",
+        graella.week,
+        ))
+    graella.setdefault('log',[]).append(logmsg)
+    schedules.save(graella)
+    schedulestorage.publishStatic(graella)
+
+@cli.command()
+@click.argument(
+    'monday',
+    type=Date,
+)
+def launch(monday):
+    import requests
+    from emili import sendMail 
+    import dbconfig
+    import time
+    import uuid
+
+    template = """\
+No he pogut generar la graella per la setmana del {monday}.
+
+- Execucio: {status.name}
+- Compleció: {status.completedCells} / {status.totalCells}
+- Penalitzacions: {status.solutionCost}
+- Cel·la de bloqueig: {status.unfilledCell}
+
+{diagnosis}
+"""
+
+    config = ns.load('config.yaml')
+    minutes = 2#*60
+
+    step(f"Timetables: Lauching timetable for {monday}")
+    execution_id = PlannerExecution.start(
+        nlines=config.nTelefons,
+        monday=str(monday),
+        description='llençada-automatica-{}'.format(uuid.uuid4()),
+    )
+
+    step(f"Timetables: Sleeping for {minutes} minutes until completition")
+    time.sleep(minutes*60)
+    step(f"Timetables: Stop sleeping after {minutes} minutes")
+
+    execution = PlannerExecution(execution_id)
+    status = execution.listInfo()
+    print(status.dump())
+
+    if status.get('status') != 'Stopped':
+        step("Timetables: Still running, stopping")
+        stop = execution.stop()
+        if not stop:
+            step("Timetables: Still running, killing")
+            kill = execution.kill()
+
+    if status.unfilledCell == "Complete":
+        step("Timetables: Complete, uploading")
+        execution.upload('tomatic')
+
+    step("Timetables: Incomplete, sending report email")
+
+    busy='\n'.join([
+        f'- **{person}:** ' + ', '.join(reasons)
+        for person, reasons in status.busyReasons.items()
+    ])
+
+    diagnosis = f"""\
+La [graella]({config.baseUrl}/api/planner/solution/{execution_id}) està incomplerta.
+El torn on m'encallo es a {status.unfilledCell}.
+
+Pots provar [llençar-la a mà]({config.baseUrl}/api/planner) però:
+
+- amb **menys línies** (sobretot si hi ha molts torns amb 'ningu')
+- indicant que vols **omplir primer el dia** amb el torn complicat
+- si encara es bloqueja, revisant les **indisponibilitats** del torn complicat
+
+Les indisponibilitats no opcionals que afecten a aquest torn son:
+
+{busy}
+""" if status.unfilledCell else f"""\
+No he arribat a farcir cap casella de la graella.
+Segurament es deu a algun error de configuració.
+
+Revisa el missatge al final de tot de [la sortida]({config.baseUrl}/api/planner/status/{execution_id}),
+i si no es res que puguis interpretar tu mateixa,
+reenvia aquest mateix correu a incidencies ({dbconfig.tomatic.supportmail}).
+"""
+
+
+    sendMail(
+        sender=dbconfig.tomatic.dailystats.sender,
+        to=dbconfig.tomatic.dailystats.recipients,
+        subject=f"ERROR: Graella setmanal sense solució {execution_id}",
+        md=template.format(
+            monday=monday,
+            execution_id=execution_id,
+            config=config,
+            status=status,
+            diagnosis=diagnosis,
+        ),
+        config='dbconfig.py',
+        verbose=True,
+    )
+
     schedules = schedulestorage.Storage()
     step("uploading {}".format(yaml))
     graella = ns.load(yaml)
