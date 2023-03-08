a3cb20a19e6324e0bd098593374702e01ca0f9a6
David García Garzón <david.garcia@somenergia.coop>
2023-03-08 08:37:08 +0100

    crontab-launchtimetable: uses non api version

--- scripts/crontab-launchtimetable.sh
+++ scripts/crontab-launchtimetable.sh
@@ -4,5 +4,5 @@ supportmail=$(python -c "import dbconfig; print(dbconfig.tomatic.supportmail)")
 appmail=$(python -c "import dbconfig; print(dbconfig.smtp.user)")
 weeksInAdvance=1
 week=$(date +%F -d "next Monday + $weeksInAdvance weeks")
-reportrun -C dbconfig.py -f $appmail -t "$supportmail" -s "Tomatic Timetable Laucher Failed" -- tomatic_timetablelauncher.py $week
+reportrun -C dbconfig.py -f $appmail -t "$supportmail" -s "Tomatic Timetable Laucher Failed" -- tomatic_timetable.py launcher.py $week
 
