2c4139aa172eb4e476830b8fe5fe2cddd75d1078
David García Garzón <david.garcia@somenergia.coop>
2023-03-09 00:14:12 +0100

    api.js prettified

--- tomatic/react-ui/src/services/api.js
+++ tomatic/react-ui/src/services/api.js
@@ -1,40 +1,56 @@
-module.exports = function() {
+module.exports = (function () {
+	var m = require('mithril')
+	var jsyaml = require('js-yaml')
+	var Auth = require('./auth')
 
-var m = require('mithril');
-var jsyaml = require('js-yaml');
-var Auth = require('./auth');
+	const debugApi = false
 
-const debugApi = false;
+	var api = {
+		request: function (options) {
+			options.config = function (xhr) {
+				xhr.setRequestHeader('Authorization', 'Bearer ' + Auth.token())
+			}
+			options.responseType = 'yaml' // whatever different of json indeed
+			options.deserialize = api.deserialize
+			debugApi &&
+				console.log(
+					options.method || 'GET',
+					options.url,
+					'Launched',
+					options.params || options.body || ''
+				)
+			return m
+				.request(options)
+				.then(function (result) {
+					debugApi &&
+						console.log(
+							options.method || 'GET',
+							options.url,
+							'Received',
+							result
+						)
+					return result
+				})
+				.catch(function (error) {
+					debugApi &&
+						console.log(
+							options.method || 'GET',
+							options.url,
+							'Error',
+							error
+						)
+					if (error.code !== 401) throw error
+					// Unauthorized
+					Auth.logout()
+				})
+		},
 
-var api = {
+		deserialize: function (responseText) {
+			return jsyaml.safeLoad(responseText)
+		},
+	}
 
-	request: function(options) {
-		options.config = function(xhr) {
-			xhr.setRequestHeader('Authorization', 'Bearer ' + Auth.token())
-		};
-		options.responseType = 'yaml'; // whatever different of json indeed
-		options.deserialize = api.deserialize;
-		debugApi && console.log(options.method || 'GET', options.url, "Launched", options.params || options.body || '');
-		return m.request(options).then(function(result) {
-				debugApi && console.log(options.method || 'GET', options.url, "Received", result);
-				return result;
-			}).catch(function(error) {
-				debugApi && console.log(options.method || 'GET', options.url, "Error", error);
-				if (error.code !== 401) throw error;
-				// Unauthorized
-				Auth.logout()
-			})
-		;
-	},
-
-	deserialize: function(responseText) {
-		return jsyaml.safeLoad(responseText);
-	},
-
-};
-
-return api;
-
-}();
+	return api
+})()
 
 // vim: noet ts=4 sw=4
