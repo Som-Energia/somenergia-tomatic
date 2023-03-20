04386b6f0e01a5b375fb4bd088abc54106a09c53
David García Garzón <david.garcia@somenergia.coop>
2023-03-20 22:48:17 +0100

    webpack configs: writting dev files, custom target, multiple entry

--- tomatic/react-ui/config-overrides.js
+++ tomatic/react-ui/config-overrides.js
@@ -1,3 +1,5 @@
+const reactAppRewireBuildDev = require('react-app-rewire-build-dev')
+const path = require('path')
 const multipleEntry = require('react-app-rewire-multiple-entry')([
         {
                 entry: 'src/admin.js',
@@ -6,9 +8,31 @@ const multipleEntry = require('react-app-rewire-multiple-entry')([
         },
 ])
 
+const options = {
+        outputPath: 'output' /***** required *****/,
+        basename: 'deploydir', // deploy react-app in a subdirectory /***** optional *****/
+        //hotReloadPort : "<port of webpack-server>" // default:3000,simply relaod the webpage on changes./***** optional *****/
+}
+
 module.exports = {
         webpack: function (config, env) {
+                console.log('NODE_ENV:', process.env.NODE_ENV)
+                const isdev = process.env.NODE_ENV == 'development'
                 multipleEntry.addMultiEntry(config)
-                return config
+                //console.log('pre config:', config)
+                config.output.path = path.resolve(
+                        __dirname,
+                        config.output.path + (isdev ? '-dev' : '')
+                )
+                return reactAppRewireBuildDev(config, env, options)
+        },
+        devServer: function (configFunction) {
+                return function (proxy, allowedHost) {
+                        const config = configFunction(proxy, allowedHost)
+                        config.devMiddleware = {
+                                writeToDisk: true,
+                        }
+                        return config
+                }
         },
 }
