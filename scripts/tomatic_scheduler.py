6d6174c394a57f0f1e272b3dedae216f0161b3a8
Marta <marta.fernandez@somenergia.coop>
2023-04-04 15:59:45 +0200

    fix: catch error when minizinc crashes 🧨

--- scripts/tomatic_scheduler.py
+++ scripts/tomatic_scheduler.py
@@ -2,12 +2,15 @@
 
 from tomatic.backtracker import main as backtracking
 from tomatic.minizinc import main as minizinc
-
+from consolemsg import error
 
 if __name__ == '__main__':
-    success = minizinc()
+    success = False
+    try:
+      success = minizinc()
+    except:
+       error("Minizinc crashed!")
     if not success:
         backtracking()
-
 
 # vim: noet
