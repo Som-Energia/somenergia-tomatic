ba48178923cc9bdec040939cd485e40fc47509c3
David García Garzón <david.garcia@somenergia.coop>
2023-09-16 06:33:15 +0200

    ✨ autofiltertype: given query detect the field

---
+++ tomatic/ui/src/services/autofiltertype.js
@@ -0,0 +1,15 @@
+export default function autofiltertype(input) {
+  switch (true) {
+    case /@/.test(input): return 'email'
+    case /^[0-9]{9}$/.test(input): return 'phone'
+    case /^[0-9]{7}$/.test(input): return 'contract'
+    case /^[ST]?[0-9]{1,6}$/.test(input): return 'soci'
+    case /^[KLMXYZ0-9][0-9]{7}[A-HJ-NP-TV-Z0-9]$/i.test(input.toUpperCase()): return 'nif'
+    case /^ES[KLMXYZ0-9][0-9]{7}[A-HJ-NP-TV-Z0-9]$/i.test(input.toUpperCase()): return 'nif'
+    case /^ES[0-9]{20}[A-Z]{2}$/i.test(input): return 'cups'
+    case /^ES[0-9]{20}[A-Z]{2}[0-9][FPRCXYZ]$/i.test(input): return 'cups'
+    case /^[0-9]+$/.test(input): return undefined
+    default: return 'name'
+  }
+}
+
---
+++ tomatic/ui/src/services/autofiltertype.spec.js
@@ -0,0 +1,49 @@
+import autofiltertype from './autofiltertype'
+
+function no() {}
+
+describe('autofiltertype detects which kind of searc we are doing', () => {
+  it('has nine numbers, should be a phone', () => {
+    expect(autofiltertype('123456789')).toBe('phone')
+  })
+  it('has seven numbers, should be a contract', () => {
+    expect(autofiltertype('1234567')).toBe('contract')
+  })
+  it('has less than seven numbers, should be a member', () => {
+    expect(autofiltertype('123456')).toBe('soci')
+  })
+  it('has a S prefix and less than seven numbers, should be a member', () => {
+    expect(autofiltertype('S123456')).toBe('soci')
+  })
+  it('has more than 9 numbers, is unknown', () => {
+    expect(autofiltertype('1234567890')).toBe(undefined)
+  })
+  it('has @, should be email', () => {
+    expect(autofiltertype('a@a.net')).toBe('email')
+  })
+  it('has eight numbers and a control letter, should be a dni, specific kind of a nif', () => {
+    expect(autofiltertype('12345678Z')).toBe('nif')
+  })
+  it('has a letter seven numbers and a control letter, should be a nif', () => {
+    expect(autofiltertype('K1234567Z')).toBe('nif')
+  })
+  it('is a lower cased nif, should also be a nif', () => {
+    expect(autofiltertype('K1234567Z')).toBe('nif')
+  })
+  it('is a lower cased nif, should also be a nif', () => {
+    expect(autofiltertype('ESK1234567Z')).toBe('nif')
+  })
+  it('has mostly letters, a name', () => {
+    expect(autofiltertype('Pepito')).toBe('name')
+  })
+  it('has cups structure, a cups', () => {
+    expect(autofiltertype('ES12341234123456789012JY')).toBe('cups')
+  })
+  it('has cups structure lowercase, a cups', () => {
+    expect(autofiltertype('es12341234123456789012jy')).toBe('cups')
+  })
+  it('has cups structure with meter point, a cups', () => {
+    expect(autofiltertype('ES12341234123456789012JY0F')).toBe('cups')
+  })
+})
+
