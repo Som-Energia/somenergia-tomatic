c8c220a73902f2e53995b6badbd8a8e258686f95
David García Garzón <david.garcia@somenergia.coop>
2023-09-22 13:24:24 +0200

    🧹 unused import

--- tomatic/ui/src/components/PersonEditor.js
+++ tomatic/ui/src/components/PersonEditor.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Dialog from '@mui/material/Dialog'
 import DialogTitle from '@mui/material/DialogTitle'
 import DialogContent from '@mui/material/DialogContent'
 import DialogActions from '@mui/material/DialogActions'
