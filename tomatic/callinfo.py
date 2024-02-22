3ab2c91d91d4af0121812479f734c28874376bfa
javikalsan <1070397+javikalsan@users.noreply.github.com>
2024-02-22 15:30:29 +0100

    ♻️  Magic number

--- tomatic/callinfo.py
+++ tomatic/callinfo.py
@@ -117,6 +117,7 @@ class CallInfo(object):
         return list(result)
 
     def partnerInfo(self, partner_data):
+        partner_category_id = 8
         result = ns(
             id_soci=self.anonymize(partner_data.ref) if partner_data.ref else "",
             lang=partner_data.lang,
@@ -134,7 +135,7 @@ class CallInfo(object):
                 ([self.anonymize(partner_data.www_mobile)] if partner_data.www_mobile else [])
             ),
             comment=partner_data.comment or '',
-            unsubscribed=8 not in partner_data.category_id,
+            unsubscribed=partner_category_id not in partner_data.category_id,
         )
         return result
 
