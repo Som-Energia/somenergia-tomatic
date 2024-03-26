6f8c78066d733c3759245648b0a5bc3a66e73623
David García Garzón <david.garcia@somenergia.coop>
2024-03-26 20:54:40 +0100

    💄 EmulateCallDialog more help to the user

--- tomatic/ui/src/components/EmulateCallDialog.js
+++ tomatic/ui/src/components/EmulateCallDialog.js
@@ -3,6 +3,8 @@ import DialogTitle from '@mui/material/DialogTitle'
 import DialogContent from '@mui/material/DialogContent'
 import DialogActions from '@mui/material/DialogActions'
 import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
+import Alert from '@mui/material/Alert'
 import TextField from '@mui/material/TextField'
 import Stack from '@mui/material/Stack'
 import AuthContext from '../contexts/AuthContext'
@@ -26,8 +28,14 @@ export default function EmulateCallDialog({ closeDialog }) {
       <DialogTitle>{'Emula trucada entrant'}</DialogTitle>
       <DialogContent>
         <Stack gap={2}>
+          <Box sx={{ p: 2 }}>
+            {
+              'Eina de proves que simula una trucada entrant com si vingués de la Centraleta.'
+            }
+          </Box>
           <TextField
             disabled
+            required
             value={extension}
             onChange={(e) => setExtension(e.target.value)}
             label={'Extensió'}
@@ -36,6 +44,7 @@ export default function EmulateCallDialog({ closeDialog }) {
             rows={4}
           />
           <TextField
+            required
             value={phoneNumber}
             onChange={(e) => setPhoneNumber(e.target.value)}
             pattern="^\d{9}$"
@@ -43,10 +52,19 @@ export default function EmulateCallDialog({ closeDialog }) {
             variant="standard"
             fullWidth
             rows={4}
+            autoFocus
           />
         </Stack>
       </DialogContent>
       <DialogActions>
+        {isValid ? (
+          <Alert sx={{opacity:0}}>{'Tot correcte'}</Alert>
+        ) : (
+          <Alert severity="error">
+            {'El número de telèfon ha de tenir 9 dígits'}
+          </Alert>
+        )}
+        <Box sx={{flex: 1}}/>
         <Button onClick={closeDialog}>{'Cancel·la'}</Button>
         <Button variant="contained" disabled={!isValid} onClick={submit}>
           {'Simula trucada'}
