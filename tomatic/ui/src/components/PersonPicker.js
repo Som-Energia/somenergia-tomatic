acba3d87fae10ffdb1d739d54e69bb0b17c805f6
David García Garzón <david.garcia@somenergia.coop>
2024-03-28 00:33:09 +0100

    🐛 PersonPicker gave linter
    - pickCell is a component PersonPickerCell
    - Removed unneeded ningu specialization

--- tomatic/ui/src/components/PersonPicker.js
+++ tomatic/ui/src/components/PersonPicker.js
@@ -3,11 +3,8 @@ import Tomatic from '../services/tomatic'
 import Grid from '@mui/material/Grid'
 import styled from '@emotion/styled'
 
-function PersonPicker(props) {
-  const { onPick, nobodyPickable } = props
 
-  var extensions = Tomatic.persons().extensions || {}
-
+function PersonPickerCell({ name, onPick }) {
   const CellItem = styled.div`
     cursor: pointer;
     text-align: center;
@@ -18,38 +15,20 @@ function PersonPicker(props) {
       opacity: 70%;
     }
   `
-  const CellItemNingu = styled.div`
-    cursor: pointer;
-    text-align: center;
-    padding: 0.5em 1em 0.5em 1em;
-    width: 10em;
-    margin: 0.3em;
-    :hover {
-      opacity: 70%;
-    }
-  `
+  return (
+    <CellItem
+      key={name}
+      className={'extension ' + name}
+      onClick={() => onPick(name)}
+    >
+      {Tomatic.formatName(name)}
+    </CellItem>
+  )
+}
 
-  function pickCell(name) {
-    if (name === 'ningu') {
-      return (
-        <CellItemNingu
-          className={'extension ' + name}
-          onClick={() => onPick(name)}
-        >
-          {Tomatic.formatName(name)}
-        </CellItemNingu>
-      )
-    }
-    return (
-      <CellItem
-        key={name}
-        className={'extension ' + name}
-        onClick={() => onPick(name)}
-      >
-        {Tomatic.formatName(name)}
-      </CellItem>
-    )
-  }
+function PersonPicker(props) {
+  const { onPick, nobodyPickable } = props
+  var extensions = Tomatic.persons().extensions || {}
   return (
     <Grid
       style={{ width: '100%', margin: 1, justifyContent: 'center' }}
@@ -58,8 +37,8 @@ function PersonPicker(props) {
     >
       {Object.keys(extensions)
         .sort()
-        .map((name) => pickCell(name))}
-      {nobodyPickable ? pickCell('ningu') : []}
+        .map((name, i) => <PersonPickerCell name={name} key={i} onPick={onPick}/>)}
+      {nobodyPickable ? <PersonPickerCell name={'ningu'} onPick={onPick}/> : []}
     </Grid>
   )
 }
