51adc2821d952d25739ec86c1c69f78f739ee3c5
David García Garzón <david.garcia@somenergia.coop>
2024-03-27 06:19:54 +0100

    ✨ BusyEditor: example includes api based, and dialog

--- tomatic/ui/src/pages/BusyPage/BusyEditor.example.js
+++ tomatic/ui/src/pages/BusyPage/BusyEditor.example.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import Container from '@mui/material/Container'
+import Paper from '@mui/material/Paper'
+import Button from '@mui/material/Button'
 import BusyEditor from './BusyEditor'
+import {BusyDialog, TomaticBusyEditor} from './BusyEditor'
 
 export default function Example() {
   const [data, setData] = React.useState({ oneshot: [], weekly: [] })
+  const [dialogPerson, setDialogPerson] = React.useState(null)
   return (
     <Container>
+      <Paper>
+      <h6>Local data BusyEditor</h6>
       <BusyEditor name={'mengano'} {...{ data, setData }} />
       <pre>{JSON.stringify(data, true, ' ')}</pre>
+      </Paper>
+      <Paper>
+        <h6>API based BusyEditor</h6>
+        <TomaticBusyEditor person="david" />
+      </Paper>
+      <Paper>
+        <h6>API based BusyDialog</h6>
+        <Button variant="contained" onClick={()=>setDialogPerson('david')}>Open as dialog</Button>
+        <BusyDialog person={dialogPerson} setPerson={setDialogPerson} />
+      </Paper>
     </Container>
   )
 }
