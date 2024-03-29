a170c322cfd1ad1ad2ac18265f1d067ce98a642d
David García Garzón <david.garcia@somenergia.coop>
2024-03-29 10:52:10 +0100

    🧹 removed dupped key attribute, unused import

--- tomatic/ui/src/pages/PbxPage/QueueMonitor.jsx
+++ tomatic/ui/src/pages/PbxPage/QueueMonitor.jsx
@@ -6,7 +6,6 @@ import DialogActions from '@mui/material/DialogActions'
 import Button from '@mui/material/Button'
 import PersonPicker from '../../components/PersonPicker'
 import { useDialog } from '../../components/DialogProvider'
-import { useSubscriptable } from '../../services/subscriptable'
 import Tomatic from '../../services/tomatic'
 
 export default function QueueMonitor() {
@@ -35,37 +34,34 @@ export default function QueueMonitor() {
 
   return (
     <Box className="queueeditor">
-      {queue.map((line) => {
-        return (
-          <Box
-            key={line.key}
-            className={[
-              'queueitem',
-              line.key,
-              line.paused ? 'paused' : 'resumed',
-              line.disconnected ? 'disconnected' : '',
-              line.ringing ? 'ringing' : '',
-              line.incall ? 'incall' : '',
-            ]}
-            onClick={() =>
-              line.paused
-                ? Tomatic.restoreLine(line.key)
-                : Tomatic.pauseLine(line.key)
-            }
-            key={line.key}
-            title={
-                (line.paused ? 'Pausada. ' : '') +
-                (line.disconnected ? 'Desconnectada. ' : '') +
-                (line.ringing ? 'Ring!! ' : '') +
-                (line.incall ? 'En Trucada. ' : '') || 'Disponible. '
-            }
-          >
-            {Tomatic.formatName(line.key)}
-            <br />
-            {Tomatic.formatExtension(line.key)}
-          </Box>
-        )
-      })}
+      {queue.map((line) => (
+        <Box
+          key={line.key}
+          className={[
+            'queueitem',
+            line.key,
+            line.paused ? 'paused' : 'resumed',
+            line.disconnected ? 'disconnected' : '',
+            line.ringing ? 'ringing' : '',
+            line.incall ? 'incall' : '',
+          ]}
+          onClick={() =>
+            line.paused
+              ? Tomatic.restoreLine(line.key)
+              : Tomatic.pauseLine(line.key)
+          }
+          title={
+            (line.paused ? 'Pausada. ' : '') +
+              (line.disconnected ? 'Desconnectada. ' : '') +
+              (line.ringing ? 'Ring!! ' : '') +
+              (line.incall ? 'En Trucada. ' : '') || 'Disponible. '
+          }
+        >
+          {Tomatic.formatName(line.key)}
+          <br />
+          {Tomatic.formatExtension(line.key)}
+        </Box>
+      ))}
       <Box className="queueitem add" onClick={addAgent}>
         {'+'}
         <br />
