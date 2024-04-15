05ca84add83431e19afb0bfa4cdb85a52e033466
David García Garzón <david.garcia@somenergia.coop>
2024-04-15 15:42:14 +0200

    🌐 Textos BusyNotes revisats per AiS

--- tomatic/ui/src/pages/BusyPage/BusyNotes.jsx
+++ tomatic/ui/src/pages/BusyPage/BusyNotes.jsx
@@ -3,17 +3,21 @@ import { MuiMarkdown } from 'mui-markdown'
 import Paper from '@mui/material/Paper'
 
 const text=`
-Marcar indisponibilitats evita que ens assignin torns a hores que no podem, o no que ens van bé, de fer.
+Marcar indisponibilitats evita que se'ns assignin torns en hores en què no podem fer atenció
 
-- Marqueu reunions, desplaçaments, conciliacions, treball concentrat, horaris atípics...
-- No cal anotar vacances, baixes i festius, que ja són a l'Odoo.
-- **Són una eina auto-gestionada per cuidar-nos**. Cuidem també a les companyes que hauran de cobrir el nostre forat.
-- Marqueu-les com a 'Opcional' sempre que sigui possible.
-    - Ens permet cobrir el torn si no hi ha ningú més disponible.
-- Aneu eliminant les que deixin de tindre sentit:
-    - Reunions de GTTs finalitzats, o d'equips on ja no hi som, portar al cole fills que ja van a la uni...
-- Indicar un motiu entenedor també ens ajuda a detectar aquests casos.
-- S'apliquen al moment de generar la graella. Si ja està generada, auto-gestioneu un canvi amb una companya.
+- Les **indisponibilitats** entraran en vigor en la següent graella que es generi.
+  Per gestionar indisponibilitats de **graelles ja publicades**, consulteu el
+  [Protocol Impossibilitat de realitzar les tasques del Servei d’Atenció i Suport](https://docs.google.com/document/d/1povOC1mF122Ed4La5YlzyxLiJyMgdIUR1FMdSSuLTVo)
+- Les indisponibilitats **són una eina auto-gestionada per cuidar-nos**;
+  cuidem també a les companyes que hauran de cobrir el nostre forat. 
+- Marqueu-les com a 'Opcional' sempre que sigui possible:
+  Ens permeten cobrir torns en cas necessari. 
+- Elimineu les indisponibilitats obsoletes:
+    - Ex: reunions de GTTs finalitzats o d'equips on ja no hi som. 
+    - Indicar un motiu entenedor també ens ajuda a detectar aquests casos.
+    - Contribueix a respectar els torns fixes
+- Les **baixes o vacances anotades a l’Odoo** ja es veuen reflexades a la graella;
+  no cal marcar-les com indisponibilitats
 `
 
 export default function BusyNotes() {
