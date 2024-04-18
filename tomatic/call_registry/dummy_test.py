1faa3fae0821326f51eb2909974c0146b95af24a
David García Garzón <david.garcia@somenergia.coop>
2024-04-18 17:56:09 +0200

    ♻️ refactor tests
    Tests were getting quite big and hard to follow.
    
    - Abstracted calls cases call_alice1, call_alice2
    - `full_call` takes default values, the id and case, and builds the
      expectation
    
    Some problems arised when i tried to reuse the case structure
    also for the expectation, since dates had different formats
    in pydantic and yaml.
    
    Also, we are stuck with Py3.9 and, until Py3.11,
    datetime.fromisoformat, does not support
    standard isoformat used in json (using Z for the tz).

--- tomatic/call_registry/dummy_test.py
+++ tomatic/call_registry/dummy_test.py
@@ -1,4 +1,6 @@
 import unittest
+import pydantic
+import datetime
 from .dummy import CallRegistry
 from .models import CallLog, NewCall
 from somutils.testutils import temp_path
@@ -7,6 +9,36 @@ from yamlns import ns
 class DummyTest(unittest.TestCase):
     from yamlns.testutils import assertNsEqual
     from somutils.testutils import enterContext
+
+    call_alice1 = ns(
+        operator = 'alice',
+        call_timestamp = "2011-11-04T00:05:23+00:00",
+        pbx_call_id = "2020-01-01T00:00:00+00::00-666555444-edade5",
+        phone_number = "666555444",
+    )
+
+    call_alice2 = ns(
+        operator = 'alice',
+        call_timestamp = "2020-01-02T00:00:00+00:00",
+        pbx_call_id = "2020-01-02T00:00:00+00:00-555444333-bababa",
+        phone_number = "555444333",
+    )
+
+    default_fields = ns(
+        caller_erp_id = None,
+        caller_vat = '',
+        caller_name = '',
+        contract_erp_id = None,
+        contract_number = '',
+        contract_address = '',
+        category_ids = [],
+        comments = '',
+    )
+
+    def full_call(self, id, data):
+        call_timestamp = datetime.datetime.fromisoformat(data.call_timestamp)
+        data = ns(data, call_timestamp = call_timestamp)
+        return ns(self.default_fields, id=id, **data)
 
     def setUp(self):
         self.maxDiff = None
@@ -24,104 +56,45 @@ class DummyTest(unittest.TestCase):
         """)
 
     def test__get_calls__after_adding_one__returns_it(self):
-        response = self.registry.add_incoming_call(NewCall(
-            operator = 'alice',
-            call_timestamp = "2020-01-01T00:00:00.000Z",
-            pbx_call_id = "2020-01-01T00:00:00.000Z-666555444-edade5",
-            phone_number = "666555444",
-        ))
+        response = self.registry.add_incoming_call(
+            NewCall(**self.call_alice1),
+        )
         odoo_id = response.odoo_id
         response = self.registry.get_calls('alice')
-        self.assertModelEqual(response, f"""
-            operator_calls:
-            - id: {odoo_id}
-              operator: alice
-              call_timestamp: 2020-01-01T00:00:00.000Z
-              pbx_call_id: 2020-01-01T00:00:00.000Z-666555444-edade5
-              phone_number: "666555444"
-              caller_erp_id: null
-              caller_vat: ''
-              caller_name: ''
-              contract_erp_id: null
-              contract_number: ''
-              contract_address: ''
-              category_ids: []
-              comments: ''
-        """)
+        self.assertModelEqual(response, ns(operator_calls=[
+            self.full_call(odoo_id, self.call_alice1),
+        ]))
 
     def test__get_calls__is_persistent_between_instances(self):
-        self.registry.add_incoming_call(NewCall(
-            operator = 'alice',
-            call_timestamp = "2020-01-01T00:00:00.000Z",
-            pbx_call_id = "2020-01-01T00:00:00.000Z-666555444-edade5",
-            phone_number = "666555444",
-        ))
+        response = self.registry.add_incoming_call(
+            NewCall(**self.call_alice1),
+        )
+        odoo_id = response.odoo_id
+
+        # Using a different CallRegistry
         other_registry = CallRegistry(self.path)
         response = other_registry.get_calls('alice')
-        self.assertModelEqual(response, """
-            operator_calls:
-            - id: 1
-              operator: alice
-              call_timestamp: 2020-01-01T00:00:00.000Z
-              pbx_call_id: 2020-01-01T00:00:00.000Z-666555444-edade5
-              phone_number: "666555444"
-              caller_erp_id: null
-              caller_vat: ''
-              caller_name: ''
-              contract_erp_id: null
-              contract_number: ''
-              contract_address: ''
-              category_ids: []
-              comments: ''
-        """)
+
+        self.assertModelEqual(response, ns(operator_calls=[
+            self.full_call(odoo_id, self.call_alice1),
+        ]))
 
     def test__get_calls__after_adding_many__returns_them(self):
-        response1 = self.registry.add_incoming_call(NewCall(
-            operator = 'alice',
-            call_timestamp = "2020-01-01T00:00:00.000Z",
-            pbx_call_id = "2020-01-01T00:00:00.000Z-666555444-edade5",
-            phone_number = "666555444",
-        ))
+        response1 = self.registry.add_incoming_call(
+            NewCall(**self.call_alice1),
+        )
         odoo_id1 = response1.odoo_id
 
-        response2 = self.registry.add_incoming_call(NewCall(
-            operator = 'alice',
-            call_timestamp = "2020-01-02T00:00:00.000Z",
-            pbx_call_id = "2020-01-02T00:00:00.000Z-555444333-bababa",
-            phone_number = "555444333",
-        ))
+        response2 = self.registry.add_incoming_call(
+            NewCall(**self.call_alice2),
+        )
         odoo_id2 = response2.odoo_id
 
         response = self.registry.get_calls('alice')
-        self.assertModelEqual(response, f"""
-            operator_calls:
-            - id: {odoo_id1}
-              operator: alice
-              call_timestamp: 2020-01-01T00:00:00.000Z
-              pbx_call_id: 2020-01-01T00:00:00.000Z-666555444-edade5
-              phone_number: "666555444"
-              caller_erp_id: null
-              caller_vat: ''
-              caller_name: ''
-              contract_erp_id: null
-              contract_number: ''
-              contract_address: ''
-              category_ids: []
-              comments: ''
-            - id: {odoo_id2}
-              operator: alice
-              call_timestamp: 2020-01-02T00:00:00.000Z
-              pbx_call_id: 2020-01-02T00:00:00.000Z-555444333-bababa
-              phone_number: "555444333"
-              caller_erp_id: null
-              caller_vat: ''
-              caller_name: ''
-              contract_erp_id: null
-              contract_number: ''
-              contract_address: ''
-              category_ids: []
-              comments: ''
-        """)
+        self.assertModelEqual(response, ns(operator_calls=[
+            self.full_call(odoo_id1, self.call_alice1),
+            self.full_call(odoo_id2, self.call_alice2),
+        ]))
 
     def test__get_calls__other_operators_calls_not_listed(self): self.skipTest("Not yet implemented")
     def test__typify_call__modifies_existing_call(self):  self.skipTest("Not yet implemented")
