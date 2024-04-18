cdaee09be389ff463a2d5ad80640f5499d6d215c
David García Garzón <david.garcia@somenergia.coop>
2024-04-18 15:53:07 +0200

    ♻️ mem->file: DUPPE: first create the new structure
    This starts a refactor to swap two equivalent implementations.
    
    First step, DUPPE: we create the new structure, the directory,
    while still keeping the old one in place (in-memory list).
    The goal of those steps is not having an unstable system
    during the refactor.

--- tomatic/call_registry/dummy.py
+++ tomatic/call_registry/dummy.py
@@ -10,10 +10,11 @@ class CallRegistry():
     def __init__(self, path: pathlib.Path):
         self.calls = []
 
+        self.registry_path = path/"call_registry"
+        self.registry_path.mkdir(exist_ok=True)
+
     def get_calls(self, operator: str):
         return CallLog(operator_calls=self.calls)
         
-        # Call(operator="caca", call_timestamp="2020-01-01T00:00:00.000Z", id=2)
-
     def add_incoming_call(self, newcall: NewCall):
         self.calls.append(Call(id=1, **newcall.model_dump()))
