7cfb766682d87ee0b49553a9394525837f479b53
Beatrigz <beatriz.plana@somenergia.coop>
2024-04-18 15:29:58 +0200

    ✅ test__get_calls__after_adding_one__returns_it
     Co-authored-by: David García Garzón <david.garcia@somenergia.coop>

--- tomatic/call_registry/dummy.py
+++ tomatic/call_registry/dummy.py
@@ -1,15 +1,19 @@
-from .models import CallLog, Call
+from .models import CallLog, Call, NewCall
 import datetime
+import pathlib
 
 class CallRegistry():
     """
     Dummy implementation for call registry using local files.
     """
 
-    def __init__(self, path):
-        pass
+    def __init__(self, path: pathlib.Path):
+        self.calls = []
 
-    def get_calls(self, operator):
-        return CallLog(operator_calls=[])
+    def get_calls(self, operator: str):
+        return CallLog(operator_calls=self.calls)
         
         # Call(operator="caca", call_timestamp="2020-01-01T00:00:00.000Z", id=2)
+
+    def add_incoming_call(self, newcall: NewCall):
+        self.calls.append(Call(id=1, **newcall.model_dump()))
--- tomatic/call_registry/dummy_test.py
+++ tomatic/call_registry/dummy_test.py
@@ -1,16 +1,56 @@
 import unittest
 from .dummy import CallRegistry
-from .models import CallLog
+from .models import CallLog, NewCall
 from somutils.testutils import temp_path
 from yamlns import ns
 
 class DummyTest(unittest.TestCase):
     from yamlns.testutils import assertNsEqual
+    from somutils.testutils import enterContext
 
-    def test__get_calls__when_no_call_registered(self):
-        with temp_path() as path:
-            registry = CallRegistry(path)
-            response: CallLog = registry.get_calls('alice')
-            self.assertNsEqual(ns(response.model_dump()), """
-                operator_calls: []
-            """)
+    def setUp(self):
+        self.path = self.enterContext(temp_path())
+        self.registry = CallRegistry(self.path)
+        return super().setUp()
+
+    def assertModelEqual(self, model, expected):
+        self.assertNsEqual(model.model_dump(), expected)
+
+    def test__get_calls__when_no_call_registered__returns_empty(self):
+        response = self.registry.get_calls('alice')
+        self.assertModelEqual(response, """
+            operator_calls: []
+        """)
+
+    def test__get_calls__after_adding_one__returns_it(self):
+        self.registry.add_incoming_call(NewCall(
+            operator = 'alice',
+            call_timestamp = "2020-01-01T00:00:00.000Z",
+            pbx_call_id = "2020-01-01T00:00:00.000Z-666555444-edade5",
+            phone_number = "666555444",
+        ))
+        response = self.registry.get_calls('alice')
+        self.assertModelEqual(response, """
+            operator_calls:
+            - id: 1
+              operator: alice
+              call_timestamp: 2020-01-01T00:00:00.000Z
+              pbx_call_id: 2020-01-01T00:00:00.000Z-666555444-edade5
+              phone_number: "666555444"
+              caller_erp_id: null
+              caller_vat: ''
+              caller_name: ''
+              contract_erp_id: null
+              contract_number: ''
+              contract_address: ''
+              category_ids: []
+              comments: ''
+        """)
+
+
+    def test__get_calls__after_adding_many__returns_them(self): self.skipTest("Not yet implemented")
+    def test__get_calls__other_operators_calls_not_listed(self): self.skipTest("Not yet implemented")
+    def test__typify_call__modifies_existing_call(self):  self.skipTest("Not yet implemented")
+    def test__typify_call__manual_call__creates_a_new_call(self):  self.skipTest("Not yet implemented")
+
+
