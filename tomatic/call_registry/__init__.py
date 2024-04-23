e72dec77ab177a13132ad2a37037c5d3d0bfe69b
David García Garzón <david.garcia@somenergia.coop>
2024-04-23 19:07:21 +0200

    🐛 smarter use of configurations

--- tomatic/call_registry/__init__.py
+++ tomatic/call_registry/__init__.py
@@ -1,17 +1,23 @@
+import os
 from . import dummy
 from . import odoo
-from pathlib import Path
-import os
-import dbconfig
+
+
+def configured_backend():
+    backend = os.environ.get('TOMATIC_CALL_REGISTRY')
+    if backend: return backend
+
+    import dbconfig
+    backend = dbconfig.tomatic.get('callregistry')
+    if backend: return backend
+
+    default_backend = 'dummy'
+    return default_backend
 
 
 # TODO: Use dbconfig to choose type and parameters.
 def CallRegistry():
-    # TODO: take it from dbconfig if not set in environment
-    default_backend = 'dummy'
-    config_backend = dbconfig.tomatic.get('callregistry')
-    env_backend = os.environ.get('TOMATIC_CALL_REGISTRY')
-    backend = env_backend or config_backend or default_backend
+    backend = configured_backend()
 
     if backend == 'dummy':
         data_path = os.environ.get('TOMATIC_DATA_PATH', 'data')
